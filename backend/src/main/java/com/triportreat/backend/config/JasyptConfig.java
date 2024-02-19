c98f7814752aa02f698a704024194a7b280e760d
honee8583 <honee8583@naver.com>
2024-02-19 19:39:05 +0900

    TOT-39 : [FEAT] 백엔드 패키지 구성
    - JasyptConfig 파일 경로 변경
    - place, region, user, plan 패키지 구성

--- backend/src/main/java/com/triportreat/backend/config/JasyptConfig.java
+++ backend/src/main/java/com/triportreat/backend/config/JasyptConfig.java
@@ -1,4 +1,4 @@
-package com.triportreat.backend;
+package com.triportreat.backend.config;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
---
+++ backend/src/main/java/com/triportreat/backend/region/entity/Region.java
@@ -0,0 +1,4 @@
+package com.triportreat.backend.region.entity;
+
+public class Region {
+}
