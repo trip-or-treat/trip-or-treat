5d338a4424ee63e5be1ed6a249d3728a2c4ac516
honee8583 <93666531+honee8583@users.noreply.github.com>
2024-02-20 21:02:30 +0900

    TOT-40 : [FEAT] JpaAuditing 기능 적용 (#10)
    * TOT-40 : [FEAT] JpaAuditing 기능 적용
    
    - JasyptConfig -> common 패키지로 이동.
    - 생성일, 수정일 필드를 갖는 BaseTimeEntity.java 생성
    - JpaAuditing 활성화 설정 파일 JpaConfig.java 생성
    
    * TOT-40 : [FEAT] BaseTimeEntity 필드명 카멜케이스로 변경

--- backend/src/main/java/com/triportreat/backend/common/config/JasyptConfig.java
+++ backend/src/main/java/com/triportreat/backend/common/config/JasyptConfig.java
@@ -1,4 +1,4 @@
-package com.triportreat.backend.config;
+package com.triportreat.backend.common.config;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
