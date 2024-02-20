652e7127b227cb00dfd50ac45202b7ea72faaafe
rlaclghks123 <rlaclghks123@naver.com>
2024-02-21 00:39:02 +0900

    TOT-11 : [CHORE] 프론트엔드 초기 세팅

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const color = {
+  mainColor: 'FC6457',
+  darkGrey: '9A9A9A',
+  lightGrey: 'D9D9D9',
+  blackFont: '3A3939',
+  lightGreyFont: 'F6F6F6',
+  whiteFont: 'FFFFFF',
+};
+
+export default { color };
