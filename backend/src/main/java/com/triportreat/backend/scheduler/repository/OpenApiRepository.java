9a50d33270e8cc1e01f0eb6a229d37ce2f481fb9
snowcrab382 <snowcrab382@gmail.com>
2024-02-21 22:45:41 +0900

    TOT-7 : [FEAT] 응답데이터 DB 저장 로직 구현

--- backend/src/main/java/com/triportreat/backend/scheduler/repository/OpenApiRepository.java
+++ backend/src/main/java/com/triportreat/backend/scheduler/repository/OpenApiRepository.java
@@ -1,8 +1,13 @@
 package com.triportreat.backend.scheduler.repository;
 
+import com.triportreat.backend.place.entity.ContentType;
+import com.triportreat.backend.place.entity.Place;
+import com.triportreat.backend.place.entity.SubCategory;
 import com.triportreat.backend.scheduler.dto.OpenApiPlaceResponse;
+import com.triportreat.backend.scheduler.dto.OpenApiPlaceResponse.Item;
 import jakarta.persistence.EntityManager;
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +17,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Repository
 @RequiredArgsConstructor
-@Transactional
 public class OpenApiRepository {
 
     private final RestTemplate restTemplate;
@@ -27,14 +31,51 @@ public class OpenApiRepository {
     @Value("${openapi.rows}")
     private Integer numOfRows;
 
+    @Transactional
     public void updatePlaceData() {
-        Integer currentPage = 1;
-        Integer totalPages = getTotalPages(currentPage);
+        Integer totalPages = getTotalPages();
 
+        updatePlacesToDatabase(totalPages);
     }
 
-    private Integer getTotalPages(Integer currentPage) {
-        URI requestUrl = setRequestUrl(numOfRows, currentPage);
+    private void updatePlacesToDatabase(Integer totalPages) {
+        for (int currentPage = 1; currentPage <= totalPages; currentPage++) {
+            URI requestUrl = setRequestUrl(numOfRows, currentPage);
+            ResponseEntity<OpenApiPlaceResponse> response = restTemplate.getForEntity(requestUrl,
+                    OpenApiPlaceResponse.class);
+
+            List<Item> responseItems = response.getBody().getResponse().getBody().getItems().getItem();
+            for (Item item : responseItems) {
+//                Region region = em.find(Region.class, item.getRegionId());
+
+                ContentType contentType = em.find(ContentType.class, item.getContentTypeId());
+
+                SubCategory subCategory = em.find(SubCategory.class, item.getSubCategoryId());
+
+                Place place = Place.builder()
+                        .id(item.getId())
+//                        .region(region)
+                        .contentType(contentType)
+                        .subCategory(subCategory)
+                        .address(item.getAddress())
+                        .addressDetail(item.getAddressDetail())
+                        .name(item.getName())
+                        .mainCategoryId(item.getMainCategoryId())
+                        .midCategoryId(item.getMidCategoryId())
+                        .imageOrigin(item.getImageOrigin())
+                        .imageThumbnail(item.getImageThumbnail())
+                        .latitude(item.getLatitude())
+                        .longitude(item.getLongitude())
+                        .sigunguCode(item.getSigunguCode())
+                        .views(0L)
+                        .build();
+                em.persist(place);
+            }
+        }
+    }
+
+    private Integer getTotalPages() {
+        URI requestUrl = setRequestUrl(numOfRows, 1);
         ResponseEntity<OpenApiPlaceResponse> response = restTemplate.getForEntity(requestUrl,
                 OpenApiPlaceResponse.class);
 
@@ -43,7 +84,7 @@ public class OpenApiRepository {
 
     }
 
-    private URI setRequestUrl(Integer numOfRows, Integer currentPage) {
-        return URI.create(baseUrl + "&serviceKey=" + serviceKey + "&numOfRows=" + numOfRows + "&pageNo=" + currentPage);
+    private URI setRequestUrl(Integer numOfRows, Integer requestPage) {
+        return URI.create(baseUrl + "&serviceKey=" + serviceKey + "&numOfRows=" + numOfRows + "&pageNo=" + requestPage);
     }
 }
