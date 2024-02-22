19f20946e3d194c5b7a4df03004ae9616a57c093
ChiHwanKim <rlaclghks123@naver.com>
2024-02-22 14:29:29 +0900

    TOT-48 : [FIX] 프론트엔드 초기세팅 문제(Theme, eslint 설정)  (#17)
    * TOT-48 : [FIX] 선언전에 스타일 컴포넌트를 사용할 수 있도록 eslint 설정 추가
    
    * TOT-48 : [FIX] theme 파일 색상 수정 및 자동완성을 위한 ts파일 추가

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,10 +1,14 @@
-export const color = {
-  mainColor: 'FC6457',
-  darkGrey: '9A9A9A',
-  lightGrey: 'D9D9D9',
-  blackFont: '3A3939',
-  lightGreyFont: 'F6F6F6',
-  whiteFont: 'FFFFFF',
+const theme = {
+  colors: {
+    mainColor: '#FC6457',
+    darkGrey: '#9A9A9A',
+    lightGrey: '#D9D9D9',
+    blackFont: '#3A3939',
+    lightGreyFont: '#F6F6F6',
+    whiteFont: '#FFFFFF',
+  },
 };
 
-export default { color };
+export type Theme = typeof theme;
+
+export default theme;
