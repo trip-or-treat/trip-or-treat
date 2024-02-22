94e10f35c36d998dfa0927f1e8fb4467e543d4fd
snowcrab382 <snowcrab382@gmail.com>
2024-02-22 19:47:01 +0900

    TOT-7 : [FEAT] validation 의존성 추가 - 공공데이터 응답결과가 null이 될 수 없도록 @NotEmpty 제약 추가

--- backend/src/main/java/com/triportreat/backend/scheduler/dto/OpenApiPlaceResponseDto.java
+++ backend/src/main/java/com/triportreat/backend/scheduler/dto/OpenApiPlaceResponseDto.java
@@ -1,14 +1,16 @@
 package com.triportreat.backend.scheduler.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class OpenApiPlaceResponse {
+public class OpenApiPlaceResponseDto {
     @JsonProperty("response")
+    @NotEmpty
     private ResponseData response;
 
     @Getter @Setter
--- backend/src/main/java/com/triportreat/backend/scheduler/service/OpenApiService.java
+++ backend/src/main/java/com/triportreat/backend/scheduler/service/OpenApiService.java
@@ -1,16 +1,64 @@
 package com.triportreat.backend.scheduler.service;
 
+import com.triportreat.backend.place.entity.ContentType;
+import com.triportreat.backend.place.entity.Place;
+import com.triportreat.backend.place.entity.SubCategory;
+import com.triportreat.backend.region.entity.Region;
+import com.triportreat.backend.scheduler.dto.OpenApiPlaceResponseDto.Item;
 import com.triportreat.backend.scheduler.repository.OpenApiRepository;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class OpenApiService {
+public class OpenApiServiceImpl implements OpenApiService{
 
     private final OpenApiRepository openApiRepository;
 
-    public void updatePlaceData() {
-        openApiRepository.updatePlaceData();
+    @Transactional
+    public void updatePlace(List<Item> items) {
+        openApiRepository.findAllRegion();
+        openApiRepository.findAllContentType();
+        openApiRepository.findAllSubCategory();
+        openApiRepository.findAllPlace();
+
+        items.stream()
+                .filter(OpenApiServiceImpl::hasForeignKeyNull)
+                .forEach(item -> {
+                    Region region = openApiRepository.findRegionById(item.getRegionId());
+                    ContentType contentType = openApiRepository.findContentTypeById(item.getContentTypeId());
+                    SubCategory subCategory = openApiRepository.findSubCategoryById(item.getSubCategoryId());
+
+                    Place place = setPlace(item, region, contentType, subCategory);
+                    openApiRepository.update(place);
+                });
+    }
+
+    private static Place setPlace(Item item, Region region, ContentType contentType, SubCategory subCategory) {
+        return Place.builder()
+                .id(item.getId())
+                .region(region)
+                .contentType(contentType)
+                .subCategory(subCategory)
+                .address(item.getAddress())
+                .addressDetail(item.getAddressDetail())
+                .name(item.getName())
+                .mainCategoryId(item.getMainCategoryId())
+                .midCategoryId(item.getMidCategoryId())
+                .imageOrigin(item.getImageOrigin())
+                .imageThumbnail(item.getImageThumbnail())
+                .latitude(item.getLatitude())
+                .longitude(item.getLongitude())
+                .sigunguCode(item.getSigunguCode())
+                .build();
+    }
+
+    private static boolean hasForeignKeyNull(Item item) {
+        return Stream.of(item.getRegionId(), item.getContentTypeId(), item.getSubCategoryId())
+                .anyMatch(Objects::isNull);
     }
 }
