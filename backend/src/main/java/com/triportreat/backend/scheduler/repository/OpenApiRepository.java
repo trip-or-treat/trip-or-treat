2277a77f0abb5e7ff94e52c75aa3d8711c3c5dfc
snowcrab382 <snowcrab382@gmail.com>
2024-02-22 20:13:49 +0900

    TOT-7 : [FEAT] OpenApiRepository 구현 - PK를 통해 엔티티를 조회하는 메서드 구현 - Place 테이블이 참조하는 테이블에 저장된 데이터를 리스트로 반환하는 메서드 구현 - Place 엔티티가 DB에 존재하지 않는다면 영속화, 존재한다면 업데이트

--- backend/src/main/java/com/triportreat/backend/scheduler/repository/OpenApiRepository.java
+++ backend/src/main/java/com/triportreat/backend/scheduler/repository/OpenApiRepository.java
@@ -3,88 +3,63 @@ package com.triportreat.backend.scheduler.repository;
 import com.triportreat.backend.place.entity.ContentType;
 import com.triportreat.backend.place.entity.Place;
 import com.triportreat.backend.place.entity.SubCategory;
-import com.triportreat.backend.scheduler.dto.OpenApiPlaceResponse;
-import com.triportreat.backend.scheduler.dto.OpenApiPlaceResponse.Item;
+import com.triportreat.backend.region.entity.Region;
 import jakarta.persistence.EntityManager;
-import java.net.URI;
+import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 @Repository
 @RequiredArgsConstructor
 public class OpenApiRepository {
 
-    private final RestTemplate restTemplate;
+    @PersistenceContext
     private final EntityManager em;
 
-    @Value("${openapi.url}")
-    private String baseUrl;
-
-    @Value("${openapi.key}")
-    private String serviceKey;
-
-    @Value("${openapi.rows}")
-    private Integer numOfRows;
-
-    @Transactional
-    public void updatePlaceData() {
-        Integer totalPages = getTotalPages();
-
-        updatePlacesToDatabase(totalPages);
+    public Region findRegionById(Long id) {
+        return em.find(Region.class, id);
     }
 
-    private void updatePlacesToDatabase(Integer totalPages) {
-        for (int currentPage = 1; currentPage <= totalPages; currentPage++) {
-            URI requestUrl = setRequestUrl(numOfRows, currentPage);
-            ResponseEntity<OpenApiPlaceResponse> response = restTemplate.getForEntity(requestUrl,
-                    OpenApiPlaceResponse.class);
+    public ContentType findContentTypeById(Long id) {
+        return em.find(ContentType.class, id);
+    }
 
-            List<Item> responseItems = response.getBody().getResponse().getBody().getItems().getItem();
-            for (Item item : responseItems) {
-//                Region region = em.find(Region.class, item.getRegionId());
+    public SubCategory findSubCategoryById(String id) {
+        return em.find(SubCategory.class, id);
+    }
 
-                ContentType contentType = em.find(ContentType.class, item.getContentTypeId());
+    public Place findPlaceById(Long id) {
+        return em.find(Place.class, id);
+    }
 
-                SubCategory subCategory = em.find(SubCategory.class, item.getSubCategoryId());
+    public List<Region> findAllRegion() {
+        return em.createQuery("select r from Region r", Region.class)
+                .getResultList();
+    }
 
-                Place place = Place.builder()
-                        .id(item.getId())
-//                        .region(region)
-                        .contentType(contentType)
-                        .subCategory(subCategory)
-                        .address(item.getAddress())
-                        .addressDetail(item.getAddressDetail())
-                        .name(item.getName())
-                        .mainCategoryId(item.getMainCategoryId())
-                        .midCategoryId(item.getMidCategoryId())
-                        .imageOrigin(item.getImageOrigin())
-                        .imageThumbnail(item.getImageThumbnail())
-                        .latitude(item.getLatitude())
-                        .longitude(item.getLongitude())
-                        .sigunguCode(item.getSigunguCode())
-                        .views(0L)
-                        .build();
-                em.persist(place);
-            }
+    public List<ContentType> findAllContentType() {
+        return em.createQuery("select c from ContentType c", ContentType.class)
+                .getResultList();
+    }
+
+    public List<SubCategory> findAllSubCategory() {
+        return em.createQuery("select s from SubCategory s", SubCategory.class)
+                .getResultList();
+    }
+
+    public List<Place> findAllPlace() {
+        return em.createQuery("select p from Place p", Place.class)
+                .getResultList();
+    }
+
+    public void update(Place place) {
+        if (findPlaceById(place.getId()) == null) {
+            em.persist(place);
+        }
+        else {
+            em.merge(place);
         }
     }
 
-    private Integer getTotalPages() {
-        URI requestUrl = setRequestUrl(numOfRows, 1);
-        ResponseEntity<OpenApiPlaceResponse> response = restTemplate.getForEntity(requestUrl,
-                OpenApiPlaceResponse.class);
-
-        Integer totalDataCount = response.getBody().getResponse().getBody().getTotalCount();
-        return (totalDataCount / numOfRows) + 1;
-
-    }
-
-    private URI setRequestUrl(Integer numOfRows, Integer requestPage) {
-        return URI.create(baseUrl + "&serviceKey=" + serviceKey + "&numOfRows=" + numOfRows + "&pageNo=" + requestPage);
-    }
 }
