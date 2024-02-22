0c7a4fd81a4a7c332640eec0d138ea71d5e33ae3
snowcrab382 <snowcrab382@gmail.com>
2024-02-22 20:05:04 +0900

    TOT-7 : [FEAT] 스케쥴러 실행을 위한 CronTable 생성 - yml에 설정한 cron 표기법에 의해 정해진 주기마다 메서드 실행 - updatePlaceData() : 장소데이터를 Dto에 저장 후 Item 부분만 Service 레이어로 전달 - getTotalCount() : 공공데이터 서버로 1회 요청하여 총 장소데이터 개수 저장 - setRequestUrl() : 공공데이터 요청 URL 생성

--- backend/src/main/java/com/triportreat/backend/scheduler/CronTable.java
+++ backend/src/main/java/com/triportreat/backend/scheduler/CronTable.java
@@ -1,24 +1,56 @@
 package com.triportreat.backend.scheduler;
 
+import com.triportreat.backend.scheduler.dto.OpenApiPlaceResponseDto;
+import com.triportreat.backend.scheduler.dto.OpenApiPlaceResponseDto.Item;
 import com.triportreat.backend.scheduler.service.OpenApiService;
+import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @RequiredArgsConstructor
 public class CronTable {
 
     private final OpenApiService openApiService;
+    private final RestTemplate restTemplate;
 
     @Value("${schedule.use}")
     private boolean isRunnable;
 
+    @Value("${openapi.url}")
+    private String baseUrl;
+
+    @Value("${openapi.key}")
+    private String serviceKey;
+
     @Scheduled(cron = "${schedule.cron}")
     public void updatePlaceData() {
         if (isRunnable) {
-            openApiService.updatePlaceData();
+            Integer totalCount = getTotalCount();
+
+            URI requestURl = setRequestUrl(totalCount, 1);
+            ResponseEntity<OpenApiPlaceResponseDto> response = restTemplate.getForEntity(requestURl,
+                    OpenApiPlaceResponseDto.class);
+
+            List<Item> items = response.getBody().getResponse().getBody().getItems().getItem();
+            openApiService.updatePlace(items);
         }
+    }
+
+    private Integer getTotalCount() {
+        URI requestUrl = setRequestUrl(1, 1);
+        ResponseEntity<OpenApiPlaceResponseDto> response = restTemplate.getForEntity(requestUrl,
+                OpenApiPlaceResponseDto.class);
+
+        return response.getBody().getResponse().getBody().getTotalCount();
+    }
+
+    private URI setRequestUrl(Integer numOfRows, Integer requestPage) {
+        return URI.create(baseUrl + "&serviceKey=" + serviceKey + "&numOfRows=" + numOfRows + "&pageNo=" + requestPage);
     }
 }
