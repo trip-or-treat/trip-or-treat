150c70e77b71aaeae3449b9381e60c8bd387c04f
ChiHwanKim <rlaclghks123@naver.com>
2024-02-23 00:24:45 +0900

    TOT-30 : [FEAT] StepNavLinkButton 구현 (#19)
    * TOT-30 : [FEAT] StepLayout 작성
    
    * TOT-30 : [FEAT] StepNavLink 구현
    
    * TOT-30 : [FEAT] StepOnePage 및 route 추가
    
    * TOT-30 : [FEAT] theme에 사용될 width,height명 추가
    
    * TOT-30 : [REFACTOR] StepNavLinkButton으로 파일명 수정
    
    * TOT-30 : [REFACTOR] StepLayout의 Main의 Height를 theme을 사용하도록 수정
    
    * TOT-30 : [REFACTOR] 버튼 타입종류명 수정

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,2 +1,10 @@
-const routes = [{}];
+import StepLayout from './layouts/StepLayout';
+import StepOnePage from './pages/StepOnePage';
+
+const routes = [
+  {
+    element: <StepLayout />,
+    children: [{ path: '/date/:region_id', element: <StepOnePage /> }],
+  },
+];
 export default routes;
