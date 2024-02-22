395accec63af53a2c75b38f3493f9baf3d9eca2d
rlaclghks123 <rlaclghks123@naver.com>
2024-02-22 15:34:58 +0900

    TOT-30 : [FEAT] StepNavLink 구현

---
+++ frontend/src/components/StepNavLink/StepNavLink.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import stepPlanSavedBtn from 'src/atoms/stepPlanSavedBtn';
+
+interface Props {
+  path: string;
+  type: 'CHANGE' | 'NOT_CHANGE';
+  children: React.ReactNode;
+}
+
+const StepNavLink = ({ path, type, children }: Props) => {
+  const isClicked = useRecoilValue(stepPlanSavedBtn);
+
+  return (
+    <Wrapper $isClicked={isClicked}>
+      {type === 'NOT_CHANGE' && <LinkBox to={path}>{children}</LinkBox>}
+      {type === 'CHANGE' && (
+        <ChangeLinkBox to={isClicked ? path : '#'} $isClicked={isClicked}>
+          {children}
+        </ChangeLinkBox>
+      )}
+    </Wrapper>
+  );
+};
+
+export default StepNavLink;
+
+const Wrapper = styled.div<{ $isClicked: boolean }>`
+  cursor: ${(props) => (props.$isClicked ? 'pointer' : 'not-allowed')};
+`;
+
+const LinkBox = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  min-width: 50px;
+  max-width: 80px;
+
+  border: none;
+  padding: 25px 20px;
+  border-radius: 8px;
+
+  background-color: ${(props) => props.theme.colors.mainColor};
+  color: ${(props) => props.theme.colors.whiteFont};
+
+  font-size: 14px;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const ChangeLinkBox = styled(LinkBox)<{ $isClicked: boolean }>`
+  background-color: ${(props) =>
+    props.$isClicked ? props.theme.colors.mainColor : props.theme.colors.darkGrey};
+  pointer-events: ${(props) => (props.$isClicked ? props.theme.colors.mainColor : 'none')};
+`;
