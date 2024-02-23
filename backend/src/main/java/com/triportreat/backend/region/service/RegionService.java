02a3f021736ff1fd65f74eb3b999a075b39d30d3
honee8583 <honee8583@naver.com>
2024-02-23 21:43:22 +0900

    TOT-32 : [FEAT] RegionService 인터페이스 작성
    - 모든 지역 목록 조회 서비스 메소드

--- backend/src/main/java/com/triportreat/backend/region/service/RegionService.java
+++ backend/src/main/java/com/triportreat/backend/region/service/RegionService.java
@@ -1,4 +1,8 @@
 package com.triportreat.backend.region.service;
 
+import com.triportreat.backend.region.domain.RegionResponseDto;
+import java.util.List;
+
 public interface RegionService {
+    List<RegionResponseDto> getRegions();
 }
