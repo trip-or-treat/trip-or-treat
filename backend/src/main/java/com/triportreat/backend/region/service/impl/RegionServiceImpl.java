bb52cf952aeee4679d1f6a59da27bc75ee483b74
honee8583 <honee8583@naver.com>
2024-02-23 21:45:23 +0900

    TOT-32 : [FEAT] RegionService 인터페이스 구현체 RegionServiceImpl 생성
    - RegionRepository의 findAll 메소드를 통해 모든 지역 데이터 조회

--- backend/src/main/java/com/triportreat/backend/region/service/impl/RegionServiceImpl.java
+++ backend/src/main/java/com/triportreat/backend/region/service/impl/RegionServiceImpl.java
@@ -1,4 +1,23 @@
 package com.triportreat.backend.region.service.impl;
 
-public class RegionServiceImpl {
+import com.triportreat.backend.region.domain.RegionResponseDto;
+import com.triportreat.backend.region.entity.Region;
+import com.triportreat.backend.region.repository.RegionRepository;
+import com.triportreat.backend.region.service.RegionService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RegionServiceImpl implements RegionService {
+
+    private final RegionRepository regionRepository;
+
+    @Override
+    public List<RegionResponseDto> getRegions() {
+        List<Region> regions = regionRepository.findAll();
+
+        return regions.stream().map(RegionResponseDto::toDto).toList();
+    }
 }
