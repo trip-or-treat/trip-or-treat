8881096ee210eaa47c0e3e6d68d43708a7443252
rlaclghks123 <rlaclghks123@naver.com>
2024-02-23 13:54:21 +0900

    TOT-14 : [FEAT] StepOne, StepTwo, StepThree Page 생성

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,16 @@
 import StepLayout from './layouts/StepLayout';
 import StepOnePage from './pages/StepOnePage';
+import StepThreePage from './pages/StepThreePage';
+import StepTwoPage from './pages/StepTwoPage';
 
 const routes = [
   {
     element: <StepLayout />,
-    children: [{ path: '/date/:region_id', element: <StepOnePage /> }],
+    children: [
+      { path: '/date/:region_id', element: <StepOnePage /> },
+      { path: '/region/:region_id', element: <StepTwoPage /> },
+      { path: '/place/:region_id', element: <StepThreePage /> },
+    ],
   },
 ];
 export default routes;
