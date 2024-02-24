4e06a08483163b4d09341cf832a709555815e876
snowcrab382 <snowcrab382@gmail.com>
2024-02-24 14:54:34 +0900

    TOT-35 : [FEAT] 장소목록 요청Dto 생성

--- backend/src/main/java/com/triportreat/backend/place/controller/PlaceController.java
+++ backend/src/main/java/com/triportreat/backend/place/controller/PlaceController.java
@@ -1,12 +1,22 @@
 package com.triportreat.backend.place.controller;
 
+import com.triportreat.backend.place.dto.PlaceRequestDto;
 import com.triportreat.backend.place.service.PlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/places")
 @RequiredArgsConstructor
 public class PlaceController {
 
     private final PlaceService placeService;
+
+    @GetMapping
+    public String getPlacesByRegionId(@ModelAttribute PlaceRequestDto placeRequestDto) {
+        return placeService.getPlacesByRegionId(placeRequestDto);
+    }
 }
