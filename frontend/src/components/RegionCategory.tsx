06569a7d9b010246f23ae397972b59a79d0adfa3
rlaclghks123 <rlaclghks123@naver.com>
2024-02-24 23:53:50 +0900

    TOT-33 : [FEAT] RegionCategory 기능 구현

---
+++ frontend/src/components/RegionCategory.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { Link, useParams } from 'react-router-dom';
+
+interface Props {
+  data: {
+    id: number;
+    name: string;
+    imageOrigin: string;
+    imageThumbnail: string;
+    latitude: number;
+    longitude: number;
+  }[];
+}
+
+const RegionCategory = ({ data }: Props) => {
+  const { regionId } = useParams();
+
+  return (
+    <Wrapper>
+      {data.map((item) => (
+        <LinkBox key={item.id} to={`/place/${item.id}`} $isClicked={item.id === Number(regionId)}>
+          <TitleBox $isClicked={item.id === Number(regionId)}>{item.name}</TitleBox>
+        </LinkBox>
+      ))}
+    </Wrapper>
+  );
+};
+
+export default RegionCategory;
+
+const Wrapper = styled.ul`
+  display: flex;
+
+  padding: 10px 0px;
+`;
+
+const LinkBox = styled(Link)<{ $isClicked: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  margin: 0px 5px;
+
+  color: ${(props) =>
+    props.$isClicked ? props.theme.colors.mainColor : props.theme.colors.darkGrey};
+`;
+
+const TitleBox = styled.p<{ $isClicked: boolean }>`
+  padding: 5px 2px;
+  margin: 0px 5px;
+
+  border-bottom: ${(props) =>
+    props.$isClicked ? `1px solid ${props.theme.colors.mainColor}` : 'none'};
+
+  font-weight: 600;
+  font-size: 27px;
+`;
