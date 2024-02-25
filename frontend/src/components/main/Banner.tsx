870e86ea2e1626d5eed27530d3e01a3ba89a4541
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-02-25 18:48:08 +0900

    TOT-15 : [FEAT] 배너 조립 완료

---
+++ frontend/src/components/main/Banner.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import mainBannerBgImg from '../../assets/images/mainBanner.png';
+import StartButton from './StartButton';
+
+const Banner = () => {
+  return (
+    <Container>
+      <BannerImg src={mainBannerBgImg} />
+      <StartButton />
+    </Container>
+  );
+};
+
+export default Banner;
+
+const Container = styled.div`
+  width: 100%;
+  margin: 80px 0px 100px 0px;
+
+  text-align: center;
+`;
+
+const BannerImg = styled.img<{ src: string }>`
+  margin: 0px 0px 90px 0px;
+
+  background-image: url(${(props) => props.src});
+  background-repeat: cover;
+`;
