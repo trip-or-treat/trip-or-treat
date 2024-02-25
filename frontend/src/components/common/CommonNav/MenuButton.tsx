c14d08692ba7db88b218a6167885d6ac9eec5ab1
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-02-25 14:39:14 +0900

    TOT-16 : [FEAT] 상단 Navbar 메뉴 버튼 구현

---
+++ frontend/src/components/common/CommonNav/MenuButton.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+interface ButtonTextProps {
+  path: string;
+  children: ReactNode;
+}
+
+const MenuButton = ({ path, children }: ButtonTextProps) => {
+  return <MenuButtonBox to={path}>{children}</MenuButtonBox>;
+};
+
+export default MenuButton;
+
+const MenuButtonBox = styled(Link)`
+  float: right;
+
+  padding: 0px 35px;
+
+  background-color: ${(props) => props.theme.colors.commonNavColor};
+
+  font-size: 20px;
+  font-family: 'Pretendard-Regular';
+  color: ${(props) => props.theme.colors.blackFont};
+  text-decoration: none;
+`;
