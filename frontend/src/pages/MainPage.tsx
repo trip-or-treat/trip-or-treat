a5b3474f949167522cbf761dcb3d63a8ebb5c596
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-02-25 20:24:09 +0900

    TOT-12 : [FEAT] 메인페이지에 지역 리스트 추가

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,7 +1,17 @@
 import Banner from 'src/components/main/Banner';
+import RegionList from 'src/components/main/RegionList/RegionList';
+
+import styled from 'styled-components';
 
 const MainPage = () => {
-  return <Banner />;
+  return (
+    <Wrapper>
+      <Banner />
+      <RegionList />
+    </Wrapper>
+  );
 };
 
 export default MainPage;
+
+const Wrapper = styled.div``;
