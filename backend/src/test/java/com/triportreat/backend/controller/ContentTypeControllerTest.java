5f1ca761803cd1ea1dbfc608d9bff41c1673a671
MisaSohee <sohee5951@naver.com>
2024-02-25 23:15:04 +0900

    TOT - 37 : [FEAT] ContentTypeController 테스트 코드 구현 - ContentTypeController의 유닛 테스트 추가 - getContentTypes() 메서드에 대한 테스트를 통해 JSON 응답이 올바르게 생성되고 반환되는지 확인 - Mock 객체를 사용하여 ContentTypeService의 실제 동작을 대신함

---
+++ backend/src/test/java/com/triportreat/backend/controller/ContentTypeControllerTest.java
@@ -0,0 +1,63 @@
+package com.triportreat.backend.controller;
+
+import com.triportreat.backend.place.controller.ContentTypeController;
+import com.triportreat.backend.place.domain.ContentTypeResponseDto;
+import com.triportreat.backend.place.service.ContentTypeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+@WebMvcTest(controllers = ContentTypeController.class)
+@AutoConfigureMockMvc
+class ContentTypeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ContentTypeService contentTypeService;
+
+    @Test
+    void getContentTypes() throws Exception {
+        // given
+        List<ContentTypeResponseDto> contentTypeResponseDtos = List.of(
+                ContentTypeResponseDto.builder().id(1L).name("type1").build(),
+                ContentTypeResponseDto.builder().id(2L).name("type2").build(),
+                ContentTypeResponseDto.builder().id(3L).name("type3").build()
+        );
+
+        // when
+        when(contentTypeService.getContentTypes()).thenReturn(contentTypeResponseDtos);
+
+        // then
+        mockMvc.perform(get("/places/content_type")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.header.status", is(200)))
+                .andExpect(jsonPath("$.header.message", is("")))
+                .andExpect(jsonPath("$.header.result", is(true)))
+                .andExpect(jsonPath("$.body", hasSize(3)))
+                .andExpect(jsonPath("$.body[0].id", equalTo(1)))
+                .andExpect(jsonPath("$.body[0].name", equalTo("type1")))
+                .andExpect(jsonPath("$.body[1].id", equalTo(2)))
+                .andExpect(jsonPath("$.body[1].name", equalTo("type2")))
+                .andExpect(jsonPath("$.body[2].id", equalTo(3)))
+                .andExpect(jsonPath("$.body[2].name", equalTo("type3")));
+    }
+}
+
+
