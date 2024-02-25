9568e742f4b53d271fb31bf796ead27c1614ef9b
snowcrab382 <snowcrab382@gmail.com>
2024-02-25 19:18:42 +0900

    TOT-35 : [FEAT] Service 구현 - Service 인터페이스와 구현체 분리 - Querydsl을 사용하는 custom repository 의존 주입 - 조회 메서드를 위한 @Transactional(readOnly = true) 옵션 사용 - 응답Dto는 List에 담아 Controller로 반환

--- backend/src/main/java/com/triportreat/backend/place/service/PlaceService.java
+++ backend/src/main/java/com/triportreat/backend/place/service/PlaceService.java
@@ -1,5 +1,10 @@
 package com.triportreat.backend.place.service;
 
-public interface PlaceService {
+import com.triportreat.backend.place.dto.PlaceByRegionIdDto;
+import com.triportreat.backend.place.dto.PlaceSearchCondition;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 
+public interface PlaceService {
+    List<PlaceByRegionIdDto> searchPlaceListByCondition(PlaceSearchCondition placeSearchCondition, Pageable pageable);
 }
--- backend/src/main/java/com/triportreat/backend/place/service/PlaceServiceImpl.java
+++ backend/src/main/java/com/triportreat/backend/place/service/PlaceServiceImpl.java
@@ -1,7 +1,25 @@
 package com.triportreat.backend.place.service;
 
+import com.triportreat.backend.place.dto.PlaceByRegionIdDto;
+import com.triportreat.backend.place.dto.PlaceSearchCondition;
+import com.triportreat.backend.place.repository.PlaceRepository;
+import com.triportreat.backend.place.repository.PlaceRepositoryCustom;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class PlaceServiceImpl implements PlaceService {
+
+    private final PlaceRepositoryCustom placeRepositoryCustom;
+    private final PlaceRepository placeRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<PlaceByRegionIdDto> searchPlaceListByCondition(PlaceSearchCondition placeSearchCondition, Pageable pageable) {
+        return placeRepositoryCustom.searchPlaceListByCondition(placeSearchCondition, pageable);
+    }
 }
