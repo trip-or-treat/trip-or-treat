980b71fdce8fa1024ff8f12714c72eb50f049010
snowcrab382 <snowcrab382@gmail.com>
2024-02-25 19:20:47 +0900

    TOT-35 : [FEAT] Repository 구현 - 기존 Spring Data JPA가 제공하는 PlaceRepository 인터페이스 사용 X - Querydsl 사용을 위한 Repository 구현체 생성(PlaceRepositoryImpl) - regionId는 필수 파라미터로 필수 조건문 추가(where절) - contentTypeId, placeName, subCategoryName은 선택 파라미터로 동적으로 조건문 추가 - contentTypeIdEquals() : 관광지타입ID값이 존재하는지 확인 후 추가 - placeNameContains(), subCategorynameContains() : keyword값이 존재하는지 확인 후 추가 - Pageable 객체를 주입하여 자동 페이징 처리

---
+++ backend/src/main/java/com/triportreat/backend/place/repository/PlaceRepositoryImpl.java
@@ -0,0 +1,72 @@
+package com.triportreat.backend.place.repository;
+
+import static com.triportreat.backend.place.entity.QContentType.contentType;
+import static com.triportreat.backend.place.entity.QPlace.place;
+import static com.triportreat.backend.place.entity.QSubCategory.subCategory;
+import static com.triportreat.backend.region.entity.QRegion.region;
+import static org.springframework.util.StringUtils.hasText;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.triportreat.backend.place.dto.PlaceByRegionIdDto;
+import com.triportreat.backend.place.dto.PlaceSearchCondition;
+import com.triportreat.backend.place.dto.QPlaceByRegionIdDto;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PlaceRepositoryImpl implements PlaceRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public PlaceRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public List<PlaceByRegionIdDto> searchPlaceListByCondition(PlaceSearchCondition placeSearchCondition, Pageable pageable) {
+        return queryFactory
+                .select(new QPlaceByRegionIdDto(
+                        place.id,
+                        place.name,
+                        place.imageThumbnail,
+                        subCategory.name,
+                        place.latitude,
+                        place.longitude,
+                        contentType.id))
+                .from(place)
+                .leftJoin(place.subCategory, subCategory)
+                .where(
+                        region.id.eq(placeSearchCondition.getRegionId()),
+                        contentTypeIdEquals(placeSearchCondition.getContentTypeId()),
+                        placeNameContains(placeSearchCondition.getKeyword()),
+                        subCategoryNameContains(placeSearchCondition.getKeyword())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private BooleanExpression placeNameContains(String keyword) {
+        if (hasText(keyword)) {
+            return place.name.contains(keyword);
+        }
+        return null;
+    }
+
+    private BooleanExpression subCategoryNameContains(String keyword) {
+        if (hasText(keyword)) {
+            return subCategory.name.contains(keyword);
+        }
+        return null;
+    }
+
+    private BooleanExpression contentTypeIdEquals(Long contentTypeId) {
+        if (contentTypeId != null) {
+            return contentType.id.eq(contentTypeId);
+        }
+        return null;
+    }
+}
