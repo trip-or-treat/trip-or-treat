60bd60695d1368159f8fb6071fc72d080bac4c8e
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-02-25 20:42:33 +0900

    TOT-12 : [REFAC] RegionName 삭제 후 RegionItem 안에 합체

--- frontend/src/components/main/RegionList/RegionItem.tsx
+++ frontend/src/components/main/RegionList/RegionItem.tsx
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
 
 import testImg from '../../../assets/images/testImg.png';
-import RegionName from './RegionName';
 
 // TODO
-// 데이터 받아와서 이미지 띄워주기
+// 데이터 받아와서 이미지와 지역명 띄워주기
 
 const RegionItem = () => {
   return (
     <Container>
       <RegionImg src={testImg} />
-      <RegionName />
+      <RegionName>서울</RegionName>
     </Container>
   );
 };
@@ -36,4 +35,16 @@ const RegionImg = styled.img<{ src: string }>`
   background-image: url(${(props) => props.src});
   border-radius: 30px;
   box-shadow: 0px 3px 5px 1px ${(props) => props.theme.colors.darkGrey};
+`;
+
+const RegionName = styled.div`
+  display: block;
+
+  margin: 20px auto;
+
+  text-align: center;
+
+  font-family: 'Pretendard-Regular';
+  font-size: 30px;
+  color: ${(props) => props.theme.colors.blackFont};
 `;
