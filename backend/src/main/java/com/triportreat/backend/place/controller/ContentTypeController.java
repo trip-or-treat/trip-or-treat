4df43c51dfbec19f8a2d3d627ee9f7071f72da34
MisaSohee <sohee5951@naver.com>
2024-02-25 17:33:12 +0900

    TOT - 37 : [REFACTOR] ContentTypeController 의 불필요한 import문 삭제

--- backend/src/main/java/com/triportreat/backend/place/controller/ContentTypeController.java
+++ backend/src/main/java/com/triportreat/backend/place/controller/ContentTypeController.java
@@ -1,18 +1,11 @@
 package com.triportreat.backend.place.controller;
 
 import com.triportreat.backend.common.response.ResponseResult;
-import com.triportreat.backend.place.entity.ContentType;
 import com.triportreat.backend.place.service.ContentTypeService;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
--- backend/src/main/java/com/triportreat/backend/place/service/ContentTypeService.java
+++ backend/src/main/java/com/triportreat/backend/place/service/ContentTypeService.java
@@ -5,5 +5,5 @@ import com.triportreat.backend.place.domain.ContentTypeResponseDto;
 import java.util.List;
 
 public interface ContentTypeService {
-    public List<ContentTypeResponseDto> getContentTypes();
+    List<ContentTypeResponseDto> getContentTypes();
 }
