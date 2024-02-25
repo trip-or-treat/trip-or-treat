c1bb2c3cf1ec0f256e60c82e5ceaf0d9e9ea6158
Yu Young MIn <88155610+yu-ratel@users.noreply.github.com>
2024-02-25 14:40:26 +0900

    TOT-28 : [FEAT] (Alert, Login)Modal 구현 (#23)
    * TOT-28 : [FEAT] LinkButton 구현
    
    * TOT-28 : [FEAT] CloseButton 구현
    
    * TOT-28 : [FEAT] LoginModal 구현
    
    * TOT-28 : [FEAT] AlertModal 구현
    
    * TOT-28 : [FIX] Model 폴더명 소문자 변경 누락 해결
    
    * TOT-28 : [REFACTOR] 모달창 외부 화면 컴포넌트화
    ModalOverlay로 구현 및 기존 코드와적용
    
    * TOT-28 : [REFACTOR] 글자간격, 라우터 위치등변경
    글자와 버튼간의 간격을 위해 동일한 margin 후 각각 위아래 margin은 0으로 간격 조절
    경고모달에서 홈으로 라우터 경로 (/)변경
    
    * TOT-28 : [STYLE] 텍스트크기, 그림자효과 변경
    텍스트 크기 15 -> 18 px
    그림자효과 아래쪽만 그림자진형태에서 오른쪽추가 후 블러효과 1px up
    
    * TOT-28 : [STYLE] 스타일 변경
    모달창 내부 텍스트 크기 15 -> 18
    폰트적용 -> semiBoid
    import 가독성을 위한 줄바꿈

---
+++ frontend/src/components/common/modal/LinkButton/LinkButton.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  path: string;
+  color: string;
+  children: React.ReactNode;
+}
+
+const LinkButton = ({ path, color, children }: Props) => {
+  return (
+    <StyledLinkButton to={path} color={color}>
+      {children}
+    </StyledLinkButton>
+  );
+};
+
+export default LinkButton;
+
+export const StyledButton = `
+  width: 68px;
+  height: 31.8px;
+  margin: 0px 15px 15px 15px;
+
+  border: none;
+  border-radius: 5px;
+  font-size: 14px;
+  font-family: 'Pretendard-SemiBold';
+
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+const StyledLinkButton = styled(Link)<{ color: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${StyledButton}
+
+  background-color: ${(props) => props.color};
+  color: ${(props) => props.theme.colors.whiteFont};
+`;
