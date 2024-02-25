1a80638adf84357ab7b873dd362728d91ba3c400
MisaSohee <sohee5951@naver.com>
2024-02-25 17:20:48 +0900

    TOT - 37 : [REFACTOR] ContentTypeService 구현을 ContentTypeServiceImple로 분리

--- backend/src/main/java/com/triportreat/backend/place/service/ContentTypeService.java
+++ backend/src/main/java/com/triportreat/backend/place/service/ContentTypeService.java
@@ -1,19 +1,9 @@
 package com.triportreat.backend.place.service;
 
-import com.triportreat.backend.place.entity.ContentType;
-import com.triportreat.backend.place.repository.ContentTypeRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.triportreat.backend.place.domain.ContentTypeResponseDto;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class ContentTypeService {
-
-    private final ContentTypeRepository contentTypeRepository;
-
-    public List<ContentType> findContentTypes() {
-        return contentTypeRepository.findAll();
-    }
+public interface ContentTypeService {
+    public List<ContentTypeResponseDto> getContentTypes();
 }
