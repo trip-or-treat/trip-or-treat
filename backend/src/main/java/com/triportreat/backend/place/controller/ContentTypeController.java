40ab71183d9c09c36ee050be4e7eae30e5b75035
MisaSohee <sohee5951@naver.com>
2024-02-25 17:30:57 +0900

    TOT - 37 : [REFACTOR] ContentTypeController의 응답 구조 수정 - ResponseEntity와 ResponseResult를 이용하도록 수정

--- backend/src/main/java/com/triportreat/backend/place/controller/ContentTypeController.java
+++ backend/src/main/java/com/triportreat/backend/place/controller/ContentTypeController.java
@@ -1,11 +1,13 @@
 package com.triportreat.backend.place.controller;
 
+import com.triportreat.backend.common.response.ResponseResult;
 import com.triportreat.backend.place.entity.ContentType;
 import com.triportreat.backend.place.service.ContentTypeService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,27 +20,9 @@ public class ContentTypeController {
 
     private final ContentTypeService contentTypeService;
 
-
     @GetMapping("/places/content_type")
-    public Result getContentTypes() {
-        List<ContentTypeDto> collect = contentTypeService.findContentTypes().stream()
-                .map(c -> new ContentTypeDto(c.getId(), c.getName()))
-                .collect(Collectors.toList());
-
-        return new Result(collect);
-    }
-
-    @Data
-    @AllArgsConstructor
-    @Builder
-    static class Result<T>{
-        private T body;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class ContentTypeDto {
-        private Long id;
-        private String name;
+    public ResponseEntity<?> getContentTypes() {
+        return ResponseEntity.ok().body(
+                ResponseResult.success("", contentTypeService.getContentTypes()));
     }
 }
