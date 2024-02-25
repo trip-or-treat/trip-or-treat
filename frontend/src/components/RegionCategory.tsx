0fac17821910b253aaf1a3614c0bfba4f10475d9
ChiHwanKim <rlaclghks123@naver.com>
2024-02-25 13:45:17 +0900

    TOT-33 : [FEAT] 지역명 카테고리 구현 (#25)
    * TOT-33 : [FEAT] RegionCategory 기능 구현
    
    * TOT-33 : [FEAT] StepThreePage에 RegionCategory 추가
    
    * TOT-33 : [STYLE] style 속성 순서에 맞게 수정
    
    * TOT-33 : [STYLE] RegionCategory 위치 수정
    
    * TOT-33 : [STYLE] PR에 디자인 수정 요청에 대한 변경적용

---
+++ frontend/src/components/RegionCategory.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import { Link, useParams } from 'react-router-dom';
+
+interface Props {
+  data: {
+    id: number;
+    name: string;
+    imageOrigin: string;
+    imageThumbnail: string;
+    latitude: number;
+    longitude: number;
+  }[];
+}
+
+const RegionCategory = ({ data }: Props) => {
+  const { regionId } = useParams();
+
+  return (
+    <Wrapper>
+      {data.map((item) => (
+        <LinkBox key={item.id} to={`/place/${item.id}`} $isClicked={item.id === Number(regionId)}>
+          <TitleBox $isClicked={item.id === Number(regionId)}>{item.name}</TitleBox>
+        </LinkBox>
+      ))}
+    </Wrapper>
+  );
+};
+
+export default RegionCategory;
+
+const Wrapper = styled.ul`
+  display: flex;
+`;
+
+const LinkBox = styled(Link)<{ $isClicked: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0px 5px;
+
+  color: ${(props) =>
+    props.$isClicked ? props.theme.colors.mainColor : props.theme.colors.darkGrey};
+
+  text-decoration: none;
+`;
+
+const TitleBox = styled.p<{ $isClicked: boolean }>`
+  padding: 5px 2px;
+  margin: 0px 10px;
+  border-bottom: ${(props) =>
+    props.$isClicked ? `1px solid ${props.theme.colors.mainColor}` : 'none'};
+
+  font-family: ${(props) => (props.$isClicked ? 'Pretendard-Semi-Bold' : 'Pretendard-Thin')};
+  font-size: 23px;
+`;
