958fb21557c08a5fa319c041f6dc7ba56bd81267
honee8583 <93666531+honee8583@users.noreply.github.com>
2024-02-25 22:58:25 +0900

    TOT-32 : [FEAT] 지역 목록 조회 API 작성 (#24)
    * TOT-32 : [FEAT] RegionService 인터페이스 작성
    - 모든 지역 목록 조회 서비스 메소드
    
    
    * TOT-32 : [FEAT] 지역 조회 Dto 생성
    - 지역 조회를 위한 Dto 클래스 RegionResponseDto 생성
    
    
    * TOT-32 : [FEAT] RegionService 인터페이스 구현체 RegionServiceImpl 생성
    - RegionRepository의 findAll 메소드를 통해 모든 지역 데이터 조회
    
    
    * TOT-32 : [FEAT] 반환할 데이터 중 헤더부분의 클래스 작성
    - ResponseHeader 클래스를 생성 후 boolean형의 성공/실패 여부, 잘못된 사항이 있을 경우 메세지를 담는 message, HttpStatus의 int값 status를 포함.
    
    
    * TOT-32 : [FEAT] 클라이언트로의 반환 클래스 ResponseResult 생성
    - ResponseHeader와 전달할 데이터를 담은 클래스 생성.
    - success 메소드를 통해 요청에 성공시 헤더(ResponseHeader)에 true, 메시지, status 값과 body필드에 전달할 데이터를 담아 생성.
    - fail 메소드를 통해 요청에 실패시 헤더에 false, 실패 메시지, status 값과 전달할 데이터를 담아 생성.
    
    
    * TOT-32 : [FEAT] 지역 컨트롤러 클래스 RegionController 생성
    - RegionController 클래스 생성 후 지역 목록 반환 컨트롤러 메소드 생성.
    - ResponseResult 객체에 헤더와 바디를 구성 후 ResponseEntity 객체에 담아 리턴.
    
    
    * TOT-32 : [FEAT] h2 데이터베이스 의존성 재주입
    - h2 데이터베이스를 테스트 환경에서 구성하기 위해 runtimeOnly -> testImplementation으로 변경
    
    
    * TOT-32 : [FEAT] application-test.yml 수정
    - 테스트용 datasource의 url 변경 (MODE=MYSQL 추가)
    - ddl-auto 옵션 제거.
    
    
    * TOT-32 : [FEAT] data.sql 충돌 해결
    - data.sql의 Region 데이터 입력 문 충돌 해결
    
    
    * TOT-32 : [FEAT] SubCategory 엔티티 클래스 name 필드 길이 변경
    - name 필드 길이 10 -> 20 변경
    
    
    * TOT-32 : [FEAT] 지역 목록 조회 메서드 트랜잭션 처리
    - getRegions 메소드 @Transactional(readOnly = true) 설정.
    
    
    * TOT-32 : [FEAT] 지역 목록 조회 테스트 코드 작성
    - RegionController 지역 목록 조회 테스트 코드 작성
      - @WebMvcTest로 컨트롤러 관련 빈만 로드
      - MockMvc를 사용해서 수행하고 RegionService를 가짜 객체로 설정.
    - RegionService 지역 목록 조회 테스트 코드 작성
      - RegionRepository를 가짜 객체로 설정.
      - assert문은 assertj 패키지를 사용. (가시성이 좋다고 함)
    - RegionRepository findAll() 메서드 테스트
      - @DataJpaTest를 사용하여 JPA레포지토리와 관련된 빈만 로드.
      - 전체 조회한 지역의 개수가 초기 데이터의 개수 17과 일치하는지 확인.
    
    
    * TOT-32 : [FEAT] RegionResponseDto의 필드명 카멜케이스로 변경
    
    
    * TOT-32 : [FEAT] RegionResponseDto 필드명 변경
    - mapX -> latitude, mapY -> longitude 로 필드명 변경.
    
    
    * TOT-32 : [FEAT] 에러 처리 핸들러 클래스 및 패키지 구조 구성 예시
    
    
    * TOT-32 : [FEAT] h2 데이터베이스 초기 데이터 삽입 sql 파일 작성
    
    
    * TOT-32 : [FEAT] 응답 형식 변경
    - ResponseResult.java 수정
    - ResponseHeader 삭제 및 해당 내용 ResponseResult와 합침.
    - 응답 성공시에는 HttpStatus.OK로 고정.
    - 응답 실패시 원인에 맞는 HttpStatus를 파라미터로 전달.
    
    
    * TOT-32 : [FEAT] 컨트롤러 테스트 코드 수정
    - 응답형식변경에 따른 컨트롤러 테스트 코드 수정

---
+++ backend/src/main/java/com/triportreat/backend/region/error/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.triportreat.backend.region.error.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+
+}
--- backend/src/main/java/com/triportreat/backend/region/service/impl/RegionServiceImpl.java
+++ backend/src/main/java/com/triportreat/backend/region/service/impl/RegionServiceImpl.java
@@ -1,4 +1,25 @@
 package com.triportreat.backend.region.service.impl;
 
-public class RegionServiceImpl {
+import com.triportreat.backend.region.domain.RegionResponseDto;
+import com.triportreat.backend.region.entity.Region;
+import com.triportreat.backend.region.repository.RegionRepository;
+import com.triportreat.backend.region.service.RegionService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RegionServiceImpl implements RegionService {
+
+    private final RegionRepository regionRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<RegionResponseDto> getRegions() {
+        List<Region> regions = regionRepository.findAll();
+
+        return regions.stream().map(RegionResponseDto::toDto).toList();
+    }
 }
