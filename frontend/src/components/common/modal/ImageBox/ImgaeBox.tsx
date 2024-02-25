e741889f748ab240609dcef176f08ac55569f948
yu-ratel <dbdudals100@naver.com>
2024-02-25 16:42:59 +0900

    TOT-31 : [STYLE] css 순서 변경

--- frontend/src/components/common/modal/ImageBox/ImgaeBox.tsx
+++ frontend/src/components/common/modal/ImageBox/ImgaeBox.tsx
@@ -14,10 +14,10 @@ const StyledImageBox = styled.div<{ imageOrigin: string }>`
   width: 457px;
   height: 217px;
 
-  border-radius: 30px;
-  box-shadow: 1px 1px 1px 0px darkgray;
-
   background-image: url(${(props) => props.imageOrigin});
   background-size: cover;
   background-position: center;
+
+  border-radius: 30px;
+  box-shadow: 1px 1px 1px 0px darkgray;
 `;
