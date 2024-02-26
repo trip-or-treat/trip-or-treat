98ff93f97de406426f3240de7fe639a11175498a
yu-ratel <dbdudals100@naver.com>
2024-02-26 19:08:53 +0900

    TOT-24 : [FEAT] 빈값이 들어올경우 보여주는 컴포넌트 구현 DefaultView

---
+++ frontend/src/components/common/modal/Overview/DefaultView.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+import goshtImage from 'src/assets/images/ghost.png';
+
+const DefaultView = () => {
+  return (
+    <StyledDefaultView>
+      정보를 불러올 수 없어요. TOT
+      <StyledGoshtImage />
+    </StyledDefaultView>
+  );
+};
+
+export default DefaultView;
+
+const StyledDefaultView = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledGoshtImage = styled.div`
+  width: 50px;
+  height: 50px;
+  margin-left: 5px;
+
+  background-size: cover;
+  background-image: url(${goshtImage});
+`;
