8544deb399ba430d97e5bb3ccc8391c9d797e4ee
ChiHwanKim <rlaclghks123@naver.com>
2024-02-27 22:23:44 +0900

    TOT-56 : [CHORE] MSW 설정 및 적용 (#39)
    * TOT-56 : [CHORE] msw 서비스워커를 public에 등록
    
    * TOT-56 : [CHORE] woker 설정
    
    * TOT-56 : [FIX] eslint prefter-default-export, no-txtraneous-dependencies 설정 off
    
    * TOT-56 : [CHORE] 개발환경에서 worker 실행코드 추가
    
    * TOT-56 : [FEAT] MSW handler, data 추가
    
    * TOT-56 : [FEAT] axios 기본설정 및 contentType fetch 함수 구현
    
    * TOT-56 : [REFACTOR] react-query 파일 분리
    
    * TOT-56 : [REFACTOR] 불필요한 문자열 제거
    
    * TOT-56 : [REFACTOR] 중복된 react-query 설정값 제거

---
+++ frontend/src/api/contentType.ts
@@ -0,0 +1,8 @@
+import { END_POINTS } from 'src/constants/api';
+import { ContentType } from 'src/@types/api/contentType';
+import { api } from '.';
+
+export const contentTypeFetcher = async () => {
+  const { data } = await api.get<ContentType[]>(END_POINTS.CONTENT_TYPE);
+  return data;
+};
