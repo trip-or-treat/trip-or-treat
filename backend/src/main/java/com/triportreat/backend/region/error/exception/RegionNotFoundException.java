b6796a62f559c085e484e3b848e7e50b36d4a5a5
honee8583 <honee8583@naver.com>
2024-02-27 18:05:06 +0900

    TOT-34 : [FEAT] 지역 데이터가 존재하지 않을경우 발생시킬 예외 클래스 생성
    - AbstractException을 상속한 RegionNotFoundException 생성.
    - BAD_REQUEST 상태코드, 예외메시지 리턴 메소드 오버라이드.

---
+++ backend/src/main/java/com/triportreat/backend/region/error/exception/RegionNotFoundException.java
@@ -0,0 +1,19 @@
+package com.triportreat.backend.region.error.exception;
+
+import static com.triportreat.backend.common.response.ErrorMessage.REGION_NOT_FOUND;
+
+import com.triportreat.backend.common.AbstractException;
+import org.springframework.http.HttpStatus;
+
+public class RegionNotFoundException extends AbstractException {
+
+    @Override
+    public HttpStatus getStatus() {
+        return HttpStatus.BAD_REQUEST;
+    }
+
+    @Override
+    public String getMessage() {
+        return REGION_NOT_FOUND.getMessage();
+    }
+}
