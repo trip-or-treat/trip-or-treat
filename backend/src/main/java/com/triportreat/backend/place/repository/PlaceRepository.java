38218c892dba7dd790e92ff5fc8f3428594b82ab
임동훈 <snowcrab382@naver.com>
2024-02-28 11:35:35 +0900

    TOT-35 : [FEAT] 특정지역별 장소목록 조회 API 구현 (#35)
    * TOT-35 : [FEAT] p6spy 의존성 추가
    - JPA에서 실제 DB로 전송하는 쿼리에 적용되는 파라미터를 확인하는 용도
    
    * TOT-35 : [FEAT] place 도메인 초기 패키지 구성
    
    * TOT-35 : [FEAT] 장소목록 요청Dto 생성
    
    * TOT-35 : [FEAT] Querydsl 의존성 추가
    
    * TOT-35 : [FEAT] 장소목록 조회 API에 필요한 요청조건 및 응답Dto 생성
    - PlaceSearchCondition : 검색조건을 파라미터로 받아 해당 객체에 저장
    - PlaceByRegionDto : 장소목록 조회 시 필요한 응답데이터 Dto
    
    * TOT-35 : [FEAT] Controller 구현
    - url 쿼리 스트링으로 전달받는 파라미터를 곧바로 PlaceSearchCondition에 저장
    - 페이징을 위한 Pageable 객체 전달(default사이즈 20 -> 10 조정)
    
    * TOT-35 : [FEAT] Service 구현
    - Service 인터페이스와 구현체 분리
    - Querydsl을 사용하는 custom repository 의존 주입
    - 조회 메서드를 위한 @Transactional(readOnly = true) 옵션 사용
    - 응답Dto는 List에 담아 Controller로 반환
    
    * TOT-35 : [FEAT] Repository 구현
    - 기존 Spring Data JPA가 제공하는 PlaceRepository 인터페이스 사용 X
    - Querydsl 사용을 위한 Repository 구현체 생성(PlaceRepositoryImpl)
    - regionId는 필수 파라미터로 필수 조건문 추가(where절)
    - contentTypeId, placeName, subCategoryName은 선택 파라미터로 동적으로 조건문 추가
    - contentTypeIdEquals() : 관광지타입ID값이 존재하는지 확인 후 추가
    - placeNameContains(), subCategorynameContains() : keyword값이 존재하는지 확인 후 추가
    - Pageable 객체를 주입하여 자동 페이징 처리
    
    * TOT-35 : [FEAT] 예외처리 및 응답형식에 맞게 변환
    
    * TOT-35 : [FEAT] 응답형식 수정
    
    * TOT-35 : [REFACTOR] 검증 관련 에러발생 반환포맷 리팩토링
    - 요청실패 관련 메세지 ENUM 클래스 생성
    - @Valid 애노테이션을 통해 검증시 발생하는 에러는 필드명과 메세지를 반환
    - 검증 필드의 default message 추가
    
    * TOT-35 :  [REFACTOR] 클래스 위치 디렉토리에 맞게 수정
    
    * TOT-35 : [TEST] 장소목록조회 API 테스트 코드 작성
    - Controller, Service, Repository 단위테스트 작성
    - 초기 장소데이터 삽입용 SQL 쿼리 추가
    
    * TOT-35 : [REFACTOR] 테스트코드 리팩토링
    - import.sql 장소 테스트데이터 삭제
    - @BeforeEach 애노테이션을 활용해 테스트데이터 주입
    - Service 검증 로직 수정
    - test시 실행되는 sql쿼리 로그를 p6spy 라이브러리로 전환
    
    ---------
    
    Co-authored-by: snowcrab382 <snowcrab382@gmail.com>

--- backend/src/main/java/com/triportreat/backend/place/repository/PlaceRepository.java
+++ backend/src/main/java/com/triportreat/backend/place/repository/PlaceRepository.java
@@ -4,4 +4,5 @@ import com.triportreat.backend.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+
 }
---
+++ backend/src/main/java/com/triportreat/backend/place/service/PlaceService.java
@@ -0,0 +1,10 @@
+package com.triportreat.backend.place.service;
+
+import com.triportreat.backend.place.domain.PlaceByRegionIdDto;
+import com.triportreat.backend.place.domain.PlaceSearchCondition;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
+public interface PlaceService {
+    List<PlaceByRegionIdDto> searchPlaceListByCondition(PlaceSearchCondition placeSearchCondition, Pageable pageable);
+}
