e09854001578a1a9da4b09a61c2583cb528b8068
honee8583 <honee8583@naver.com>
2024-02-29 20:34:12 +0900

    TOT-52 : [CHORE] main, develop 브랜치마다 다른 프로파일 적용 후 배포 테스트

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -3,20 +3,61 @@
 REPOSITORY=/home/ubuntu/triportreat
 cd $REPOSITORY
 
-APP_NAME=triportreat
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+# 실행되고 있는 운영서버 PID
+MAIN_PID=$(pgrep -f production)
+# 실행되고 있는 개발 서버 PID
+DEV_PID=$(pgrep -f develop)
 
-if [ -z $CURRENT_PID ]
+# 정상 실행 로그 파일
+MAIN_LOG_OUT="/home/ubuntu/logs/main.out"
+DEV_LOG_OUT="/home/ubuntu/logs/dev.out"
+# 에러 로그 파일
+MAIN_LOG_ERR="/home/ubuntu/logs/main.err"
+DEV_LOG_ERR="/home/ubuntu/logs/dev.err"
+
+
+# 혹시 개발서버가 실행중일 경우 종료
+if [ -n $DEV_PID ]; 
 then
-  echo "> 종료할것 없음."
-else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+	echo "개발서버 실행중...종료합니다"
+	kill -15 $DEV_PID
+	sleep 5
+else 
+	echo "개발서버가 실행중이 아닙니다"
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar -Dspring.profiles.active=develop $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+# 메인브랜치로 푸시될 경우
+# TODO main브랜치로 수정
+if [ "$GITHUB_REF" = "refs/heads/CHORE/TOT-52" ]; then
+	echo "메인브랜치가 푸시되었습니다. 메인 프로파일로 실행합니다."
+
+  # 이전에 실행된 프로세스의 PID를 찾아 종료
+  if [ -n $MAIN_PID ]; 
+	then
+    echo "메인 프로세스를 종료합니다..."
+    kill -15 $MAIN_PID
+    sleep 5
+  else
+    echo "메인 프로세스가 실행중이 아닙니다..."
+  fi
+	
+	echo "메인 프로세스를 실행합니다..."
+	nohup java -jar -Dspring.profiles.active=production $JAR_PATH >$MAIN_LOG_OUT 2>$MAIN_LOG_ERR < /dev/null &
+
+else
+  # 개발 브랜치로 푸시될 경우
+  echo "개발브랜치가 푸시되었습니다. 개발 프로파일로 잠시 실행합니다."
+  
+  # 프로세스 시작
+  echo "develop 프로파일로 실행합니다..."
+  nohup java -jar -Dspring.profiles.active=develop $JAR_PATH >$DEV_LOG_OUT 2>$DEV_LOG_ERR < /dev/null &
+  
+  # 프로세스 시작 후 바로 종료
+  echo "개발 프로세스를 종료합니다..."
+	kill -15 $DEV_PID
+	sleep 5
+fi
