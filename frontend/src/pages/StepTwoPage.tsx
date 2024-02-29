b9b624ba4ffea97ff5abf76f2d3c4704fbb769ac
rlaclghks123 <rlaclghks123@naver.com>
2024-02-29 20:08:14 +0900

    TOT-57 : [FEAT] StepTwoPage에 MyRegionList, EnterSearch, SearchRegionList 조합

--- frontend/src/pages/StepTwoPage.tsx
+++ frontend/src/pages/StepTwoPage.tsx
@@ -1,5 +1,52 @@
+import styled from 'styled-components';
+
+import EnterSearch from 'src/components/EnterSearch';
+import MyRegionList from 'src/components/MyRegionList';
+import SearchRegionList from 'src/components/SearchRegionList';
+
 const StepTwoPage = () => {
-  return <div>StepTwoPage</div>;
+  return (
+    <Wrapper>
+      <SearchLayer>
+        <MyRegionList />
+
+        <section>
+          <EnterSearch placeHolder="여행지를 검색해보세요!" />
+        </section>
+
+        <SearchRegionList />
+      </SearchLayer>
+
+      <MapLayer>mapLayer</MapLayer>
+    </Wrapper>
+  );
 };
 
 export default StepTwoPage;
+
+const Wrapper = styled.div`
+  display: flex;
+
+  width: calc(100vw - ${(props) => props.theme.width.leftNavWidth});
+  height: calc(100vh - ${(props) => props.theme.height.topNavHeight});
+`;
+
+const SearchLayer = styled.div`
+  width: 30%;
+  height: inherit;
+  border-left: ${(props) => `1px solid ${props.theme.colors.lightGrey}`};
+  border-right: ${(props) => `1px solid ${props.theme.colors.lightGrey}`};
+
+  box-sizing: border-box;
+
+  section {
+    padding: 0px 20px;
+  }
+`;
+
+const MapLayer = styled.div`
+  width: 70%;
+  height: inherit;
+
+  background-color: darkgoldenrod;
+`;
