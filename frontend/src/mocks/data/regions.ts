4eba7ba6397ccc25be56696ec32869a524803aa8
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-03-01 16:49:20 +0900

    TOT-12 : [REFAC] mock data에 썸네일 이미지 경로 삽입

--- frontend/src/mocks/data/regions.ts
+++ frontend/src/mocks/data/regions.ts
@@ -4,136 +4,170 @@ export const regionsData: Regions[] = [
   {
     id: 1,
     name: '서울',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 2,
     name: '인천',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 3,
     name: '대전',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1703442701300-cb06c58378a2?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1703442701300-cb06c58378a2?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 4,
     name: '대구',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 5,
     name: '광주',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 6,
     name: '부산',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 7,
     name: '울산',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 8,
     name: '세종특별자치시',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 9,
     name: '경기도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 10,
     name: '강원특별자치도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 11,
     name: '충청북도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 12,
     name: '충청남도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 13,
     name: '경상북도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 14,
     name: '경상남도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 15,
     name: '전북특별자치도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1705615427946-847174bd8cf3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231231,
     longitude: 35.1231231231,
   },
   {
     id: 16,
     name: '전라남도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1706078256287-e436e97559c2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
   {
     id: 17,
     name: '제주도',
-    imageOrigin: '원본이미지 S3 저장경로',
-    imageThumbnail: '썸네일이미지 S3 저장경로',
+    imageOrigin:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageThumbnail:
+      'https://images.unsplash.com/photo-1698627163545-2a27b1ccaab2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     latitude: 123.1231231232,
     longitude: 35.1231231232,
   },
