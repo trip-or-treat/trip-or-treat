e4ccf8c5ef4aaeaaf6d70003525b2caafe3c47bc
yu-ratel <dbdudals100@naver.com>
2024-03-02 15:20:12 +0900

    TOT-29 : [FEAT] Place 컴포넌트 구현

---
+++ frontend/src/components/RecommendPlaces/Place.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+interface Props {
+  name: string;
+  overview: string;
+  imageThumbnail: string;
+}
+
+const Place = ({ name, overview, imageThumbnail }: Props) => {
+  return (
+    <Wrapper>
+      <StyledImageThumbnail imageThumbnail={imageThumbnail} />
+      <StyledTextInner>
+        <StyledName>{name}</StyledName>
+        <StyledOverview>{overview.slice(0, overview.indexOf('.') + 1)}</StyledOverview>
+      </StyledTextInner>
+    </Wrapper>
+  );
+};
+
+export default Place;
+
+const Wrapper = styled.div`
+  display: flex;
+
+  margin: 30px;
+`;
+
+const StyledTextInner = styled.div`
+  display: flex;
+  flex-flow: column;
+
+  margin-left: 15px;
+`;
+
+const StyledName = styled.h2`
+  margin: 15px 0px 15px 0px;
+
+  font-family: 'Pretendard-SemiBold';
+`;
+
+const StyledOverview = styled.p`
+  font-family: 'Pretendard-Thin';
+`;
+
+const StyledImageThumbnail = styled.div<{ imageThumbnail: string }>`
+  flex: 0 0 auto;
+
+  width: 111px;
+  height: 108px;
+
+  background-image: url(${(props) => props.imageThumbnail});
+  background-size: cover;
+  background-position: center;
+  border-radius: 30px;
+`;
