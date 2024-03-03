a9fa9f89fa34556c8200f15464129ba2068bec48
rlaclghks123 <rlaclghks123@naver.com>
2024-03-03 22:13:43 +0900

    TOT-59 : [REFACTOR] postman 제한으로 인한 임시 데이터 사용을 위해 수정

--- frontend/src/components/PlaceList/PlaceList.tsx
+++ frontend/src/components/PlaceList/PlaceList.tsx
@@ -1,28 +1,122 @@
 import { useRef } from 'react';
 import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+// import { useParams } from 'react-router-dom';
+// import { useRecoilValue } from 'recoil';
 
 import { PlaceListTypes } from 'src/@types/api/placeList';
-import useInfinityScroll from 'src/hooks/api/useInfinityScroll';
-import contentTypeIdAtom from 'src/atoms/contentTypeIdAtom';
+// import useInfinityScroll from 'src/hooks/api/useInfinityScroll';
+// import contentTypeIdAtom from 'src/atoms/contentTypeIdAtom';
 import PlaceCard from './PlaceCard';
 
 interface Props {
   keyword: string;
 }
 
+const data = [
+  {
+    id: 11,
+    name: '울산',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 12,
+    name: '부산',
+    imageThumbnail: '',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 13,
+    name: '포항',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 14,
+    name: '서울',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 15,
+    name: '부산',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 16,
+    name: '포항',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 17,
+    name: '서울',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 18,
+    name: '부산',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+  {
+    id: 19,
+    name: '포항',
+    imageThumbnail:
+      'https://plus.unsplash.com/premium_photo-1661948404806-391a240d6d40?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFRCU5NSU5QyVFQSVCNSVBRCUyMCVFQyU5NyVBQyVFRCU5NiU4OXxlbnwwfHwwfHx8MA%3D%3D',
+    subCategoryName: '산',
+    latitude: 123.1231231231,
+    longitude: 35.1231231231,
+    contentTypeId: 12,
+  },
+];
+
 const PlaceList = ({ keyword }: Props) => {
   const observerRef = useRef(null);
-  const { regionId } = useParams();
-  const prevContentTypeId = useRecoilValue(contentTypeIdAtom);
+  // const { regionId } = useParams();
+  // const prevContentTypeId = useRecoilValue(contentTypeIdAtom);
 
-  const { data, isLoading } = useInfinityScroll({
-    observerRef,
-    regionId,
-    keyword,
-    prevContentTypeId,
-  });
+  // const { data, isLoading } = useInfinityScroll({
+  //   observerRef,
+  //   regionId,
+  //   keyword,
+  //   prevContentTypeId,
+  // });
+  console.log(keyword);
+  const isLoading = false;
 
   return (
     <>
@@ -32,11 +126,10 @@ const PlaceList = ({ keyword }: Props) => {
 
         {!isLoading && (
           <>
-            {data?.pages?.map((items) =>
-              items?.data.map((placeCardItem: PlaceListTypes) => (
-                <PlaceCard key={placeCardItem.id} placeCardItem={placeCardItem} type="ADD_BUTTON" />
-              )),
-            )}
+            {/* {data?.pages?.map((items) => */}
+            {data.map((placeCardItem: PlaceListTypes) => (
+              <PlaceCard key={placeCardItem.id} placeCardItem={placeCardItem} type="ADD_BUTTON" />
+            ))}
             <div ref={observerRef}>
               <Loading />
             </div>
