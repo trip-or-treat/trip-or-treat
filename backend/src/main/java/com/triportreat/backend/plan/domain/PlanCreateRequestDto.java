a2f2cde1ec0a5de2d43371157cbf561f2453d345
honee8583 <honee8583@naver.com>
2024-03-03 15:51:16 +0900

    TOT-54 : [FEAT] Plan, Schedule, SchedulePlace 저장을 위한 Dto클래스 생성 및 validation 설정
    - Plan 저장을 위한 PlanCreateRequestDto 클래스 생성.
    - Schedule 저장을 위한 ScheduleCreateRequestDto 클래스 생성.
    - SchedulePlace 저장을 위한 SchedulePlaceCreateRequestDto 클래스 생성.
    - 각 클래스에 validation을 위한 어노테이션과 에러메시지 작성.

---
+++ backend/src/main/java/com/triportreat/backend/plan/domain/PlanCreateRequestDto.java
@@ -0,0 +1,35 @@
+package com.triportreat.backend.plan.domain;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PlanCreateRequestDto {
+
+    @NotEmpty(message = "제목은 필수 입력값입니다!")
+    private String title;
+
+    @NotNull(message = "여행 시작날짜는 필수 입력값입니다!")
+    private LocalDate startDate;
+
+    @NotNull(message = "여행 종료날짜는 필수 입력값입니다!")
+    private LocalDate endDate;
+
+    @NotNull(message = "사용자아이디는 필수 입력값입니다!")
+    private Long userId;
+
+    @Valid
+    @Size(min = 1, message = "스케쥴은 최소 하루는 있어야 합니다!")
+    private List<ScheduleCreateRequestDto> schedules = new ArrayList<>();
+}
