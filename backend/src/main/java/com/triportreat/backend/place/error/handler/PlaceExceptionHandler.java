a88dc3d62eb59bff5973bd998c7c204fad64cd19
MisaSohee <sohee5951@naver.com>
2024-03-03 16:20:54 +0900

    TOT-51 : [FEAT] PlaceExceptionHandler에 새로운 예외 처리 메서드 추가 - 장소 정보 조회 실패, 외부 API 호출 실패, 외부 API 응답 처리 실패의 상황을 대응하기 위한 새로운 예외 처리 메서드 구현 - placeNotFoundExceptionHandler: PlaceNotFoundException 예외를 처리. 예외의 메시지와 상태를 반환. - apiCallFailedExceptionHandler: ApiCallFailedException 예외를 처리. 예외의 메시지와 상태를 반환. - apiResponseParseExceptionHandler: ApiResponseParseException 예외를 처리. 예외의 메시지와 상태를 반환.

--- backend/src/main/java/com/triportreat/backend/place/error/handler/PlaceExceptionHandler.java
+++ backend/src/main/java/com/triportreat/backend/place/error/handler/PlaceExceptionHandler.java
@@ -1,19 +1,23 @@
 package com.triportreat.backend.place.error.handler;
 
-import static com.triportreat.backend.common.response.FailMessage.GET_FAIL;
-import static com.triportreat.backend.common.response.FailMessage.UNKNOWN_ERROR;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-
 import com.triportreat.backend.common.response.ResponseResult;
 import com.triportreat.backend.place.controller.PlaceController;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.triportreat.backend.place.error.handler.exception.ApiCallFailedException;
+import com.triportreat.backend.place.error.handler.exception.ApiResponseParseException;
+import com.triportreat.backend.place.error.handler.exception.PlaceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.triportreat.backend.common.response.FailMessage.GET_FAIL;
+import static com.triportreat.backend.common.response.FailMessage.UNKNOWN_ERROR;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice(basePackageClasses = PlaceController.class)
 public class PlaceExceptionHandler {
@@ -29,6 +33,22 @@ public class PlaceExceptionHandler {
                 .stream()
                 .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
         return ResponseEntity.ok().body(ResponseResult.fail(GET_FAIL.getMessage(), BAD_REQUEST, errors));
+    }
+
+    @ExceptionHandler(PlaceNotFoundException.class)
+    protected ResponseEntity<?> placeNotFoundExceptionHandler(PlaceNotFoundException e) {
+        return ResponseEntity.ok()
+                .body(ResponseResult.fail(e.getMessage(), e.getStatus(), null));
+    }
+
+    @ExceptionHandler(ApiCallFailedException.class)
+    protected ResponseEntity<?> apiCallFailedExceptionHandler(ApiCallFailedException e) {
+        return ResponseEntity.ok().body(ResponseResult.fail(e.getMessage(), e.getStatus(), null));
+    }
+
+    @ExceptionHandler(ApiResponseParseException.class)
+    protected ResponseEntity<?> apiResponseParseExceptionHandler(ApiResponseParseException e) {
+        return ResponseEntity.ok().body(ResponseResult.fail(e.getMessage(), e.getStatus(), null));
     }
 
 }
