61a6a16a692c65eaaa985e3bb4cf75e85c3975f6
snowcrab382 <snowcrab382@gmail.com>
2024-03-04 14:40:30 +0900

    TOT-86 :  [FIX] 허용 url을 ,로 구분하여 재작성

--- backend/src/main/java/com/triportreat/backend/common/config/WebConfig.java
+++ backend/src/main/java/com/triportreat/backend/common/config/WebConfig.java
@@ -10,15 +10,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://localhost:3000")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("https://localhost:8080")
-                .allowedOrigins("http://triportreat.site")
-                .allowedOrigins("https://triportreat.site")
-                .allowedOrigins("http://www.triportreat.site")
-                .allowedOrigins("https://www.triportreat.site")
-                .allowedOrigins("https://test-cors.org")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "http://localhost:8080",
+                        "https://localhost:8080",
+                        "http://triportreat.site",
+                        "https://triportreat.site",
+                        "http://www.triportreat.site",
+                        "https://www.triportreat.site",
+                        "https://test-cors.org")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
