576b3745463b92508ec2dcd21ba79310d47b9fbe
임동훈 <snowcrab382@naver.com>
2024-03-05 13:28:22 +0900

    TOT-86 :  [FIX] CORS 에러 핸들링 (#53)
    * TOT-86 :  [FIX] Origin Url 추가 후 테스트
    
    * TOT-86 :  [FIX] 허용 url을 ,로 구분하여 재작성
    
    * TOT-86 :  [FIX] github action 스크립트 복구 및 테스트 origin url 삭제
    
    ---------
    
    Co-authored-by: snowcrab382 <snowcrab382@gmail.com>

--- backend/src/main/java/com/triportreat/backend/common/config/WebConfig.java
+++ backend/src/main/java/com/triportreat/backend/common/config/WebConfig.java
@@ -10,11 +10,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("https://triportreat.site")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "http://localhost:8080",
+                        "https://localhost:8080",
+                        "http://triportreat.site",
+                        "https://triportreat.site",
+                        "http://www.triportreat.site",
+                        "https://www.triportreat.site")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
