b3caf81eaad3805b14279b67f43998c0e2bd78c8
rlaclghks123 <rlaclghks123@naver.com>
2024-03-07 17:46:57 +0900

    TOT-20 : [FEAT] : PlansList 기능 구현

--- frontend/src/atoms/totalPlanAtom.ts
+++ frontend/src/atoms/totalPlanAtom.ts
@@ -14,13 +14,9 @@ const { persistAtom } = recoilPersist();
 const totalPlanAtom = atom<TotalPlan[]>({
   key: 'totalPlanAtom',
   default: [
-    { day: 1, date: '02 21', items: [] },
-    { day: 2, date: '02 22', items: [] },
-    { day: 3, date: '02 23', items: [] },
-    { day: 4, date: '02 24', items: [] },
-    { day: 5, date: '02 25', items: [] },
-    { day: 6, date: '02 26', items: [] },
-    { day: 7, date: '02 27', items: [] },
+    { day: 1, date: '2024-02-13', items: [] },
+    { day: 2, date: '2024-02-22', items: [] },
+    { day: 3, date: '2024-02-23', items: [] },
   ],
   effects_UNSTABLE: [persistAtom],
 });
---
+++ frontend/src/components/PlansList/PlansList.tsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+
+import totalPlanAtom from 'src/atoms/totalPlanAtom';
+import { useCarousel } from 'src/hooks/useCarousel';
+import PlansListCard from './PlansListCard';
+
+import { ReactComponent as ChevronLeft } from '../../assets/svgs/chevronLeft.svg';
+import { ReactComponent as ChevronRight } from '../../assets/svgs/chevronRight.svg';
+
+const PlansList = () => {
+  const totalPlan = useRecoilValue(totalPlanAtom);
+  const { handlePrev, handleNext, slideRef } = useCarousel(totalPlan.length % 2, 323);
+
+  return (
+    <>
+      <PlansListWrapper ref={slideRef} $length={totalPlan.length}>
+        {totalPlan.map((item, listIdx) => (
+          <Wrapper key={item.day}>
+            <DayTitle>{`${item.day}일차`}</DayTitle>
+
+            {item.items.map((data, idx) => (
+              <PlansListCard key={data.id} listIdx={listIdx} idx={idx} data={data} />
+            ))}
+          </Wrapper>
+        ))}
+      </PlansListWrapper>
+
+      <LeftButton onClick={handlePrev} $length={totalPlan.length}>
+        <ChevronLeft />
+      </LeftButton>
+      <RightButton onClick={handleNext} $length={totalPlan.length}>
+        <ChevronRight />
+      </RightButton>
+    </>
+  );
+};
+
+export default PlansList;
+
+const Wrapper = styled.div`
+  overflow: auto;
+  width: 323px;
+  height: 58vh;
+`;
+
+const DayTitle = styled.p`
+  font-size: 16px;
+  font-family: 'Pretendard-SemiBold';
+  color: ${(props) => props.theme.colors.blackFont};
+`;
+
+const MoveButtonBox = styled.button<{ $length: number }>`
+  display: ${(props) => (props.$length <= 4 ? 'none' : 'flex')};
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+
+  border: none;
+  background-color: inherit;
+
+  cursor: pointer;
+
+  svg {
+    width: 15px;
+    fill: ${(props) => props.theme.colors.blackFont};
+  }
+`;
+
+const PlansListWrapper = styled.div<{ $length: number }>`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: ${(props) => (props.$length <= 4 ? 'center' : 'none')};
+
+  margin-top: 25px;
+`;
+
+const LeftButton = styled(MoveButtonBox)`
+  left: 25px;
+`;
+
+const RightButton = styled(MoveButtonBox)`
+  right: 25px;
+`;
