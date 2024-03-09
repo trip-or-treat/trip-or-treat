0c9af3487efda3e3832cf43a333517fc566cd7de
MisaSohee <sohee5951@naver.com>
2024-03-09 15:02:17 +0900

    TOT-77 : [REFACTOR] ReviewController 수정

--- backend/src/main/java/com/triportreat/backend/place/controller/ReviewController.java
+++ backend/src/main/java/com/triportreat/backend/place/controller/ReviewController.java
@@ -3,12 +3,11 @@ package com.triportreat.backend.place.controller;
 import com.triportreat.backend.common.response.ResponseResult;
 import com.triportreat.backend.place.service.ReviewListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.triportreat.backend.common.response.SuccessMessage.GET_SUCCESS;
@@ -20,8 +19,7 @@ public class ReviewController {
     private final ReviewListService reviewListService;
 
     @GetMapping("places/{id}/review")
-    public ResponseEntity<?> getReviewList(@PathVariable Long id, @RequestParam(defaultValue = "0") int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+    public ResponseEntity<?> getReviewList(@PathVariable Long id, @PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok().body(ResponseResult.success(GET_SUCCESS.getMessage(), reviewListService.getReviewList(id, pageable)));
     }
 }
