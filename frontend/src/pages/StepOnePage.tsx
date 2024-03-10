e8c26e1befbd40ee02a559bb4725ef6562b069fd
Yu Young MIn <88155610+yu-ratel@users.noreply.github.com>
2024-03-10 20:52:17 +0900

    TOT-69 : [FEAT] step1 날짜생성 구현 (#75)
    * TOT-69: [STYLE] 달력 전역 스타일링
    
    * TOT-69: [FEAT] 캘린더 컴포넌트 구현
    
    * TOT-69: [FEAT] 날짜모달 구현
    
    * TOT-69 : [FEAT] stpe1 페이지 모달연결

--- frontend/src/pages/StepOnePage.tsx
+++ frontend/src/pages/StepOnePage.tsx
@@ -1,15 +1,26 @@
 import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
 
 import myRegionListAtom from 'src/atoms/myRegionListAtom';
+import KaKaoMap from 'src/components/KaKaoMap';
+import ScheduleModal from 'src/components/ScheduleModal';
 
 const StepOnePage = () => {
   const myRegionList = useRecoilValue(myRegionListAtom);
 
   return (
-    <div>
-      <div>{myRegionList[0]?.name}</div>
-    </div>
+    <Wrapper>
+      <ScheduleModal />
+      <KaKaoMap list={myRegionList} />
+    </Wrapper>
   );
 };
 
 export default StepOnePage;
+
+const Wrapper = styled.div`
+  display: flex;
+
+  width: calc(100vw - ${(props) => props.theme.width.leftNavWidth});
+  height: calc(100vh - ${(props) => props.theme.height.topNavHeight});
+`;
