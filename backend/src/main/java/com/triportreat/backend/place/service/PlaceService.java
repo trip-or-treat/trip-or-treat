ca120eede157806a0f2c908abc0854580bf25062
임동훈 <snowcrab382@naver.com>
2024-03-10 18:42:15 +0900

    TOT-95 : [REFACTOR] 장소 데이터 업데이트 로직 개선 (#72)
    * TOT-95 : [REFACTOR] 장소데이터 갱신 코드 리팩토링 및 패키지 경로 수정
    - OpenApiService 구현체들을 ExternalApiService로 통합
    - 장소데이터 갱신 로직을 Spring Data JPA를 이용한 코드로 리팩토링
    - 장소 관광지타입ID가 축제공연행사(15) 또는 여행코스(25)이면 저장하지 않도록 필터 추가
    - mapx -> longitude, mapy -> latitude로 필드명 매핑 수정
    
    * TOT-95 : [TEST] 외부API 호출, 장소 업데이트 로직 테스트코드 작성
    - 외부API 호출실패시 예외, 성공시 리스트로 장소데이터 반환 테스트
    - 장소 업데이트시 기존 장소 업데이트, 새 장소 저장, 필터링 테스트
    
    ---------
    
    Co-authored-by: snowcrab382 <snowcrab382@gmail.com>

--- backend/src/main/java/com/triportreat/backend/place/service/PlaceService.java
+++ backend/src/main/java/com/triportreat/backend/place/service/PlaceService.java
@@ -3,12 +3,13 @@ package com.triportreat.backend.place.service;
 import com.triportreat.backend.place.domain.PlaceByRegionIdDto;
 import com.triportreat.backend.place.domain.PlaceInfoDto;
 import com.triportreat.backend.place.domain.PlaceSearchCondition;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface PlaceService {
     List<PlaceByRegionIdDto> searchPlaceListByCondition(PlaceSearchCondition placeSearchCondition, Pageable pageable);
 
     PlaceInfoDto getPlaceInfo(Long id);
+  
+    void updatePlacesByExternalApi();
 }
