6e49756813ed0dbaf4bf0bd00b7f88e4022ae22b
snowcrab382 <snowcrab382@gmail.com>
2024-03-17 12:22:06 +0900

    TOT-55 : [FEAT] 인증 서비스 핵심 로직 구현 - 요청으로 전달받은 카카오 인가코드를 이용해 카카오 엑세스토큰 발급 - 발급받은 토큰으로 사용자 정보 획득 - 사용자 email 기준으로 DB에 존재하면 반환, 존재하지 않으면 저장 후 반환 - 사용자ID를 토큰정보에 담아 토큰 생성 - 응답에 토큰 저장

---
+++ backend/src/main/java/com/triportreat/backend/auth/service/AuthService.java
@@ -0,0 +1,96 @@
+package com.triportreat.backend.auth.service;
+
+import com.triportreat.backend.auth.JwtProvider;
+import com.triportreat.backend.auth.domain.KakaoLoginRequestDto;
+import com.triportreat.backend.auth.domain.KakaoTokenResponseDto;
+import com.triportreat.backend.auth.domain.KakaoUserInfoResponseDto;
+import com.triportreat.backend.auth.domain.KakaoUserInfoResponseDto.KakaoAccount;
+import com.triportreat.backend.user.entity.User;
+import com.triportreat.backend.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
+    private final JwtProvider jwtProvider;
+
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${kakao.token-uri}")
+    private String tokenUri;
+
+    @Value("${kakao.user-info-uri}")
+    private String userInfoUri;
+
+    public void loginByKakao(KakaoLoginRequestDto loginRequestInfo, HttpServletResponse response) {
+        //카카오 엑세스토큰 발급
+        String kakaoAccessToken = getKakaoAccessToken(loginRequestInfo.getCode());
+
+        //카카오 사용자정보 조회
+        KakaoAccount userInfo = getUserInfo(kakaoAccessToken);
+
+        //사용자 정보를 토대로 신규 유저면 저장, 기존 유저면 조회
+        User user = getUser(userInfo);
+
+        //해당 유저에 대해 JWT 토큰 발급
+        String accessToken = jwtProvider.issueAccessToken(user.getId());
+        String refreshToken = jwtProvider.issueRefreshToken(user.getId());
+
+        //응답 헤더와 쿠키에 토큰 저장
+        jwtProvider.setAccessAndRefreshToken(response, accessToken, refreshToken);
+    }
+
+    public User getUser(KakaoAccount userInfo) {
+        if (!isAlreadyUser(userInfo)) {
+            return userRepository.save(User.toEntity(userInfo));
+        }
+        return userRepository.findByEmail(userInfo.getEmail());
+    }
+
+    public boolean isAlreadyUser(KakaoAccount userInfo) {
+        return userRepository.existsByEmail(userInfo.getEmail());
+    }
+
+    public KakaoAccount getUserInfo(String kakaoAccessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setBearerAuth(kakaoAccessToken);
+
+        KakaoUserInfoResponseDto response = restTemplate
+                .postForObject(userInfoUri, new HttpEntity<>(headers), KakaoUserInfoResponseDto.class);
+
+        return response.getKakaoAccount();
+    }
+
+    public String getKakaoAccessToken(String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", clientId);
+        params.add("redirect_uri", redirectUri);
+        params.add("code", code);
+
+        KakaoTokenResponseDto response = restTemplate
+                .postForObject(tokenUri, new HttpEntity<>(params, headers), KakaoTokenResponseDto.class);
+
+        return response.getAccessToken();
+    }
+}
