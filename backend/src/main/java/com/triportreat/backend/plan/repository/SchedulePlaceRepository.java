ae093c0197b8e33911db13eef3c0ba62579ede06
snowcrab382 <snowcrab382@gmail.com>
2024-03-20 22:33:05 +0900

    TOT-131 :  [FEAT] fetch join을 이용해 스케쥴-장소 조회 시 plan 엔티티까지 조회하는 메서드 구현

--- backend/src/main/java/com/triportreat/backend/plan/repository/SchedulePlaceRepository.java
+++ backend/src/main/java/com/triportreat/backend/plan/repository/SchedulePlaceRepository.java
@@ -1,8 +1,16 @@
 package com.triportreat.backend.plan.repository;
 
 import com.triportreat.backend.plan.entity.SchedulePlace;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SchedulePlaceRepository extends JpaRepository<SchedulePlace, Long> {
+
+    @Query("SELECT sp FROM SchedulePlace sp "
+            + "LEFT JOIN FETCH sp.schedule s "
+            + "LEFT JOIN FETCH s.plan p "
+            + "WHERE sp.id = :id AND p.user.id = :userId")
+    Optional<SchedulePlace> findByIdAndUserId(Long id, Long userId);
 
 }
