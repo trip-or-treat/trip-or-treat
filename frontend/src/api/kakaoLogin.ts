aad1269be132b9ed1c506b8e8ffc12787bb9491b
yu-ratel <dbdudals100@naver.com>
2024-03-20 20:04:30 +0900

    TOT-115 : [REFACTOR] login api return값 변경 바로 엑세스토큰을 가져올 수 있도록 변경

--- frontend/src/api/kakaoLogin.ts
+++ frontend/src/api/kakaoLogin.ts
@@ -1,6 +1,6 @@
 import { api } from '.';
 
 export const kakaoLoginFetcher = async (code: string | null) => {
-  const { data } = await api.get(`/login?code=${code}`);
-  return data;
+  const data = await api.get(`/login?code=${code}`);
+  return data.headers.authorization;
 };
