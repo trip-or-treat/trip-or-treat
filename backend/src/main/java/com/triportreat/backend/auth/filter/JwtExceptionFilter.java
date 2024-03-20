e68fa87adb5a6781321d906dd7aae7d5ef95242a
임동훈 <snowcrab382@naver.com>
2024-03-20 12:23:21 +0900

    Revert "TOT-130 : [REVERT] 필터 계층 응답 헤더 cors 설정 수정 (#104)"
    This reverts commit 1dd17274b236ee8a1bd49726e32dfa70d907bc8b.

--- backend/src/main/java/com/triportreat/backend/auth/filter/JwtExceptionFilter.java
+++ backend/src/main/java/com/triportreat/backend/auth/filter/JwtExceptionFilter.java
@@ -8,14 +8,11 @@ import com.triportreat.backend.common.response.ResponseResult;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
@@ -27,23 +24,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class JwtExceptionFilter implements Filter {
 
-    private static HashSet<String> allowedOrigin = new HashSet<>();
-
     private final ObjectMapper objectMapper;
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-
-        allowedOrigin.add("http://localhost:3000");
-        allowedOrigin.add("https://localhost:3000");
-        allowedOrigin.add("http://localhost:8080");
-        allowedOrigin.add("https://localhost:8080");
-        allowedOrigin.add("http://triportreat.site");
-        allowedOrigin.add("https://triportreat.site");
-        allowedOrigin.add("http://www.triportreat.site");
-        allowedOrigin.add("https://www.triportreat.site");
-    }
 
     @Override
     public void doFilter(
@@ -58,24 +39,28 @@ public class JwtExceptionFilter implements Filter {
             chain.doFilter(request, response);
         } catch (JwtException e) {
             log.info("예외 필터에서 예외 캐치");
-            jwtExceptionHandler((HttpServletRequest) request, (HttpServletResponse) response, e);
+            jwtExceptionHandler((HttpServletResponse) response, e);
         }
     }
 
-    public void jwtExceptionHandler(HttpServletRequest request, HttpServletResponse response, JwtException e) {
-        String origin = request.getHeader("Origin");
-        if (allowedOrigin.contains(origin)) {
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            response.setStatus(OK.value());
-
-            response.setHeader("Access-Control-Allow-Origin", origin);
-            response.setHeader("Access-Control-Allow-Headers", "*");
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-            response.setHeader("Access-Control-Max-Age", "3600");
-        }
-        
+    public void jwtExceptionHandler(HttpServletResponse response, JwtException e) {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(OK.value());
+        response.setHeader("Access-Control-Allow-Origin",
+                "http://localhost:3000, "
+                        + "https://localhost:3000, "
+                        + "http://localhost:8080, "
+                        + "https://localhost:8080, "
+                        + "http://triportreat.site, "
+                        + "https://triportreat.site, "
+                        + "http://www.triportreat.site, "
+                        + "https://www.triportreat.site");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         try {
             ResponseResult result = ResponseResult.fail(e.getMessage(), UNAUTHORIZED, null);
             String jsonResponse = objectMapper.writeValueAsString(result);
