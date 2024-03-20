599a47c4b898907e3de3b46002d38fdb90b35015
snowcrab382 <snowcrab382@gmail.com>
2024-03-20 21:45:41 +0900

    TOT-93 : [FEAT] 인증 관련 예외 및 핸들러 구현 - 요청 데이터와 사용자가 불일치한 경우 예외발생 - 인증이 필요한 여러 도메인에서 사용되므로 global 핸들러로 위치

---
+++ backend/src/main/java/com/triportreat/backend/common/error/exception/AuthenticateFailException.java
@@ -0,0 +1,19 @@
+package com.triportreat.backend.common.error.exception;
+
+import static com.triportreat.backend.common.response.FailMessage.AUTHENTICATION_FAILED;
+
+import com.triportreat.backend.common.AbstractException;
+import org.springframework.http.HttpStatus;
+
+public class AuthenticateFailException extends AbstractException {
+
+    @Override
+    public HttpStatus getStatus() {
+        return HttpStatus.BAD_REQUEST;
+    }
+
+    @Override
+    public String getMessage() {
+        return AUTHENTICATION_FAILED.getMessage();
+    }
+}
