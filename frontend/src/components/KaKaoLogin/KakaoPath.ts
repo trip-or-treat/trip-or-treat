ca78b252845938c560aed93bc65c300dce7ee5a4
yu-ratel <dbdudals100@naver.com>
2024-03-20 21:27:21 +0900

    TOT-115 : [FEAT] kakaoLogin 공통 nav 연결 logout시 동작을 위해 MenuButton Props 추가

--- frontend/src/components/KaKaoLogin/KakaoPath.ts
+++ frontend/src/components/KaKaoLogin/KakaoPath.ts
@@ -1,3 +1,4 @@
+// 개발완료시 배포 도메인으로 변경
 const REDIRECT_URI = 'https://localhost:3000/login';
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
 
