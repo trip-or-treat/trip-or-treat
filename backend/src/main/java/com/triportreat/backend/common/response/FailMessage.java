12e194bb816b922b12c499a763e467f3fd9d0db6
MisaSohee <sohee5951@naver.com>
2024-03-21 22:42:55 +0900

    TOT-105 : [FEAT] 성공 및 실패 메세지 추가

--- backend/src/main/java/com/triportreat/backend/common/response/FailMessage.java
+++ backend/src/main/java/com/triportreat/backend/common/response/FailMessage.java
@@ -12,7 +12,9 @@ public enum FailMessage {
     USER_NOT_FOUND("사용자가 존재하지 않습니다!"),
     PLACE_NOT_FOUND("장소가 존재하지 않습니다!"),
     VALIDATION_FAILED("유효성 검증에 실패하였습니다!"),
-    PLAN_NOT_FOUND("계획이 존재하지 않습니다!");
+    PLAN_NOT_FOUND("계획이 존재하지 않습니다!"),
+    REVIEW_NOT_FOUND("리뷰가 존재하지 않습니다!"),
+    REVIEW_NOT_BELONG_TO_PLACE("리뷰가 해당 장소에 속하지 않습니다!");
 
     private final String message;
 
--- backend/src/main/java/com/triportreat/backend/common/response/SuccessMessage.java
+++ backend/src/main/java/com/triportreat/backend/common/response/SuccessMessage.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum SuccessMessage {
         GET_SUCCESS("조회에 성공하였습니다."),
-        POST_SUCCESS("저장에 성공하였습니다.");
+        POST_SUCCESS("저장에 성공하였습니다."),
+        PUT_SUCCESS("수정에 성공하였습니다.");
 
         private final String message;
 
