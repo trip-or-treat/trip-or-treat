f29badce19ff27d90744d19c7e24afa9ec3a6ebe
honee8583 <honee8583@naver.com>
2024-03-23 18:05:09 +0900

    TOT-80 : [FEAT] 내 계획 목록 검색 및 조회 컨트롤러 메서드 작성

--- backend/src/main/java/com/triportreat/backend/plan/controller/PlanController.java
+++ backend/src/main/java/com/triportreat/backend/plan/controller/PlanController.java
@@ -8,9 +8,12 @@ import static com.triportreat.backend.plan.domain.PlanRequestDto.PlanUpdateReque
 import com.triportreat.backend.auth.utils.Auth;
 import com.triportreat.backend.common.response.ResponseResult;
 import com.triportreat.backend.plan.domain.PlanRequestDto.PlanCreateRequestDto;
+import com.triportreat.backend.plan.domain.PlanRequestDto.PlanSearchRequestDto;
 import com.triportreat.backend.plan.service.PlanService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -30,6 +33,12 @@ public class PlanController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getPlanDetail(@Auth Long userId, @PathVariable("id") Long planId) {
         return ResponseEntity.ok().body(ResponseResult.success(GET_SUCCESS.getMessage(), planService.getPlanDetail(planId, userId)));
+    }
+
+    @GetMapping("/plans")
+    public ResponseEntity<?> searchPlans(PlanSearchRequestDto condition, @PageableDefault(size = 10) Pageable pageable) {
+        Long userId = 1L;   // TODO 사용자정보를 불러올 수 있을경우 수정 필요
+        return ResponseEntity.ok().body(ResponseResult.success(GET_SUCCESS.getMessage(), planService.searchPlans(condition, pageable, userId)));
     }
 
     @PostMapping
