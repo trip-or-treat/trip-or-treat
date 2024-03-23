9bb3a0b93c64b1d8c026fbca1b379a4ed3be0004
MisaSohee <sohee5951@naver.com>
2024-03-23 10:44:27 +0900

    TOT-105 : [FEAT] 불필요한 입력 삭제

--- backend/src/main/java/com/triportreat/backend/place/domain/ReviewUpdateRequestDto.java
+++ backend/src/main/java/com/triportreat/backend/place/domain/ReviewUpdateRequestDto.java
@@ -2,21 +2,22 @@ package com.triportreat.backend.place.domain;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
 @Builder
-@NoArgsConstructor(force = true)
+@NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class ReviewUpdateRequestDto {
 
     @NotNull(message = "placeId는 필수입니다.")
-    private final Long placeId;
+    private  Long placeId;
     @NotEmpty(message = "내용은 필수 입력값입니다.")
-    private final String content;
-    private final String tip;
+    private  String content;
+    private  String tip;
     @NotNull(message = "별점은 필수 입력값입니다.")
-    private final Integer score;
+    private  Integer score;
 }
