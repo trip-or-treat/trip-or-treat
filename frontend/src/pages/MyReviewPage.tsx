21e044bf28d36fad04b954511b7d277d44a5fe7c
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-03-23 13:42:50 +0900

    TOT-111 : [FEAT] 내 리뷰목록 페이지 구현

---
+++ frontend/src/pages/MyReviewPage.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+import MyPageTitle from 'src/components/mypage/MyPageTitle';
+import ReviewListCategory from 'src/components/mypage/ReviewListCategory';
+import MyReviewList from 'src/components/mypage/MyReviewList';
+
+const MY_REVIEW_DATA = [
+  {
+    placeId: '1',
+    review: '사람이 너무너무 많아요 근데 볼거리가 진짜 많아서 추천해요!',
+    place: '망원 한강공원ㅇㅇㅇㅇㅇㅇㅇㅇ',
+    score: 5,
+    createdAt: '2024-03-14',
+  },
+  {
+    placeId: '2',
+    review: '사람이 너무너무 많아요 근데 볼거리가 진짜 많아서 추천해요!',
+    place: '망원 한강공원',
+    score: 5,
+    createdAt: '2024-03-14',
+  },
+  {
+    placeId: '3',
+    review: '사람이 너무너무 많아요 근데 볼거리가 진짜 많아서 추천해요!',
+    place: '망원 한강공원',
+    score: 5,
+    createdAt: '2024-03-14',
+  },
+  {
+    placeId: '4',
+    review: '사람이 너무너무 많아요 근데 볼거리가 진짜 많아서 추천해요!',
+    place: '망원 한강공원',
+    score: 5,
+    createdAt: '2024-03-14',
+  },
+  {
+    placeId: '5',
+    review: '사람이 너무너무 많아요 근데 볼거리가 진짜 많아서 추천해요!',
+    place: '망원 한강공원',
+    score: 5,
+    createdAt: '2024-03-14',
+  },
+  {
+    placeId: '6',
+    review: '사람이 너무너무 많아요 근데 볼거리가 진짜 많아서 추천해요!',
+    place: '망원 한강공원',
+    score: 5,
+    createdAt: '2024-03-14',
+  },
+];
+
+const MyReviewPage = () => {
+  return (
+    <Wrapper>
+      <MyPageTitle>내 리뷰 목록</MyPageTitle>
+      <ReviewListCategory />
+      {MY_REVIEW_DATA.map((data) => {
+        return (
+          <MyReviewList
+            key={data.placeId}
+            review={data.review}
+            place={data.place}
+            score={data.score}
+            createdAt={data.createdAt}
+          />
+        );
+      })}
+    </Wrapper>
+  );
+};
+
+export default MyReviewPage;
+
+const Wrapper = styled.div`
+  width: 750px;
+`;
