cbaa5c0f1d1be61f5a346fc1b546bcf6ff6079bf
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-03-23 13:34:34 +0900

    TOT-111 : [FEAT] 리뷰리스트, 계획목록에 필요한 공통 컨테이너 css 구현

---
+++ frontend/src/components/mypage/CommonContainer.ts
@@ -0,0 +1,12 @@
+export const CommonContainer = `
+  display: flex;
+  align-items: center;
+
+  height: 45px;
+  padding: 0 20px 0 15px;
+
+  font-family: 'Pretendard-Regular';
+  font-size: 15px;
+  color: ${`(props) => props.theme.colors.blackFont`};
+  text-align: center;
+`;
