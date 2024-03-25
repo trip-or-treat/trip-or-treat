767d08887e89d8c472915a9753ddc4579e54992d
snowcrab382 <snowcrab382@gmail.com>
2024-03-25 15:40:48 +0900

    TOT-84 :  [FEAT] Repository 구현

--- backend/src/main/java/com/triportreat/backend/plan/repository/PlanRepository.java
+++ backend/src/main/java/com/triportreat/backend/plan/repository/PlanRepository.java
@@ -1,8 +1,11 @@
 package com.triportreat.backend.plan.repository;
 
 import com.triportreat.backend.plan.entity.Plan;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+    Optional<Plan> findByCode(String code);
 
 }
