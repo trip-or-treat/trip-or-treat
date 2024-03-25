cf9d7aefe2bcc7e948929f883bc902b9cdebe211
honee8583 <honee8583@naver.com>
2024-03-25 15:17:34 +0900

    TOT-133 : [REFACTOR] 계획상세정보 조회 테스트 코드 리팩토링
    - DummyObject에 목객체 생성문 추가.

--- backend/src/test/java/com/triportreat/backend/dummy/DummyObject.java
+++ backend/src/test/java/com/triportreat/backend/dummy/DummyObject.java
@@ -2,8 +2,10 @@ package com.triportreat.backend.dummy;
 
 import com.triportreat.backend.place.domain.ReviewRequestDto;
 import com.triportreat.backend.place.domain.ReviewUpdateRequestDto;
+import com.triportreat.backend.place.entity.ContentType;
 import com.triportreat.backend.place.entity.Place;
 import com.triportreat.backend.place.entity.Review;
+import com.triportreat.backend.place.entity.SubCategory;
 import com.triportreat.backend.plan.domain.PlanRequestDto.PlanCreateRequestDto;
 import com.triportreat.backend.plan.domain.PlanRequestDto.PlanUpdateRequestDto;
 import com.triportreat.backend.plan.domain.PlanRequestDto.ScheduleCreateRequestDto;
@@ -11,12 +13,15 @@ import com.triportreat.backend.plan.domain.PlanRequestDto.SchedulePlaceCreateReq
 import com.triportreat.backend.plan.domain.PlanRequestDto.SchedulePlaceUpdateRequestDto;
 import com.triportreat.backend.plan.domain.PlanRequestDto.ScheduleUpdateRequestDto;
 import com.triportreat.backend.plan.entity.Plan;
+import com.triportreat.backend.plan.entity.PlanRegion;
 import com.triportreat.backend.plan.entity.Schedule;
 import com.triportreat.backend.plan.entity.SchedulePlace;
+import com.triportreat.backend.region.entity.Region;
 import com.triportreat.backend.user.entity.User;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public class DummyObject {
 
@@ -30,29 +35,77 @@ public class DummyObject {
                 .build();
     }
 
-    protected Plan createMockPlan(Long id, List<Schedule> schedules) {
+    protected Plan createMockPlan(Long id) {
+        return createMockPlan(id, null, null, null, null);
+    }
+
+    protected Plan createMockPlan(Long id, Set<PlanRegion> regions, List<Schedule> schedules, LocalDate startDate, LocalDate endDate) {
         return Plan.builder()
                 .id(id)
                 .title("Plan")
                 .schedules(schedules)
+                .regions(regions)
+                .startDate(startDate)
+                .endDate(endDate)
                 .build();
     }
 
-    protected Schedule createMockSchedule(Long id, LocalDate visitDate, List<SchedulePlace> schedulePlaces) {
+    protected Schedule createMockSchedule(Long id, Plan plan, List<SchedulePlace> schedulePlaces, LocalDate visitDate) {
         return Schedule.builder()
                 .id(id)
+                .plan(plan)
                 .schedulePlaces(schedulePlaces)
                 .visitDate(visitDate)
                 .build();
     }
 
-    protected SchedulePlace createMockSchedulePlace(Long id, Long placeId, int visitOrder) {
+    protected SchedulePlace createMockSchedulePlace(Long id, Place place, Schedule schedule, int visitOrder) {
         return SchedulePlace.builder()
                 .id(id)
-                .place(Place.builder().id(placeId).build())
+                .place(place)
+                .schedule(schedule)
                 .visitOrder(visitOrder)
                 .memo("memo")
                 .expense(1000L)
+                .build();
+    }
+
+    protected Region createMockRegion(Long id, String name) {
+        return Region.builder()
+                .id(id)
+                .name(name)
+                .imageOrigin("imageOrigin")
+                .imageThumbnail("imageThumbnail")
+                .latitude(1.1)
+                .longitude(1.1)
+                .overview("overview")
+                .build();
+    }
+
+    protected PlanRegion createPlanRegion(Long id, Plan plan, Region region) {
+        return PlanRegion.builder()
+                .id(id)
+                .plan(plan)
+                .region(region)
+                .build();
+    }
+
+    protected Place createMockPlace(Long id, Region region, ContentType contentType, SubCategory subCategory, String name) {
+        return Place.builder()
+                .id(id)
+                .region(region)
+                .contentType(contentType)
+                .subCategory(subCategory)
+                .address("address")
+                .addressDetail("addressDetail")
+                .name(name)
+                .mainCategoryId("mainCategoryId")
+                .midCategoryId("midCategoryId")
+                .imageOrigin("imageOrigin")
+                .imageThumbnail("imageThumbnail")
+                .latitude(1.1)
+                .longitude(1.1)
+                .sigunguCode(1)
                 .build();
     }
 
