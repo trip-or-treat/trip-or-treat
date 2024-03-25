1c7c45d22e73c9b80afb7c7f11e2879fd2c688a9
snowcrab382 <snowcrab382@gmail.com>
2024-03-25 15:38:48 +0900

    TOT-84 :  [FEAT] Controller 구현

--- backend/src/main/java/com/triportreat/backend/plan/controller/PlanController.java
+++ backend/src/main/java/com/triportreat/backend/plan/controller/PlanController.java
@@ -13,20 +13,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/plans")
 @RequiredArgsConstructor
 public class PlanController {
 
     private final PlanService planService;
 
-    @GetMapping("/plans/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> getPlanDetail(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(ResponseResult.success(GET_SUCCESS.getMessage(), planService.getPlanDetail(id)));
     }
 
-    @PostMapping("/plans")
+    @GetMapping("/share/{code}")
+    public ResponseEntity<?> getSharedPlanDetail(@PathVariable("code") String code) {
+        return ResponseEntity.ok().body(ResponseResult.success(GET_SUCCESS.getMessage(), planService.getSharedPlanDetail(code)));
+    }
+
+    @PostMapping
     public ResponseEntity<?> createPlan(@RequestBody @Valid PlanCreateRequestDto planCreateRequestDto) {
         planCreateRequestDto.setUserId(1L); // TODO 로그인 기능 구현 완료시 수정 필요
         planService.createPlan(planCreateRequestDto);
