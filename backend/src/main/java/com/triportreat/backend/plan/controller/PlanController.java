eb102f95fccf787f86e2d79b9773b6f0b10efe5f
honee8583 <honee8583@naver.com>
2024-03-27 03:20:16 +0900

    TOT-133 : [REFACTOR] Slf4j 제거

--- backend/src/main/java/com/triportreat/backend/plan/controller/PlanController.java
+++ backend/src/main/java/com/triportreat/backend/plan/controller/PlanController.java
@@ -11,7 +11,6 @@ import com.triportreat.backend.plan.domain.PlanRequestDto.PlanCreateRequestDto;
 import com.triportreat.backend.plan.service.PlanService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
