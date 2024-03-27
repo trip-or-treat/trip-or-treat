69848ff81b078d6fabf574da0809cbca4694ba87
kyejin0412@kookmin.ac.kr <kyejin0412@kookmin.ac.kr>
2024-03-27 15:20:01 +0900

    TOT-112 : [FEAT] 삭제 아이콘 클릭 시 확인 모달 띄워주기

--- frontend/src/components/mypage/MyPlanList/MyPlanList.tsx
+++ frontend/src/components/mypage/MyPlanList/MyPlanList.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
+import AlertModal from 'src/components/AlertModal';
 import { CommonContainer } from '../../../styles/CommonContainer';
 import { ReactComponent as DeleteIcon } from '../../../assets/svgs/delete.svg';
 
@@ -13,16 +16,34 @@ interface Props {
 }
 
 const MyPlanList = ({ idx, title, regions, startDate, endDate, createdDate }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <PlanContainer>
       <Index>{idx}</Index>
-      <PlanTitle>{title}</PlanTitle>
+      {/* 제목 클릭하면 mypage/plans/:planId 페이지로 보내주기 */}
+      <PlanTitle to="/mypage/plans/:planId">{title}</PlanTitle>
       <Region>{regions.join(' ')}</Region>
       <Period>
         {startDate} ~ {endDate}
       </Period>
       <CreatedAt>{createdDate}</CreatedAt>
-      <DeleteIcon />
+      <DeleteIcon onClick={() => setOpen(true)} />
+      {open && (
+        <AlertModal
+          onButtonText="삭제"
+          offButtonText="취소"
+          path="/mypage/myPlan"
+          onClose={onClose}
+          // onClick={삭제 api}
+        >
+          정말 삭제하시겠어요?
+        </AlertModal>
+      )}
     </PlanContainer>
   );
 };
@@ -34,10 +55,10 @@ const PlanContainer = styled.div`
 
   border-bottom: solid 1px ${(props) => props.theme.colors.lightGrey};
 
-  cursor: pointer;
-
   svg {
     width: 28px;
+
+    cursor: pointer;
 
     &:hover {
       fill: ${(props) => props.theme.colors.mainColor};
@@ -47,27 +68,47 @@ const PlanContainer = styled.div`
 
 const Index = styled.div`
   width: 40px;
+
   text-align: center;
+
+  cursor: default;
 `;
 
-const PlanTitle = styled.div`
+const PlanTitle = styled(Link)`
   width: 215px;
   padding-left: 20px;
+
+  color: ${(props) => props.theme.colors.blackFont};
+  text-decoration: none;
+
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: solid 1px ${(props) => props.theme.colors.blackFont};
+  }
 `;
 
 const Region = styled.div`
   width: 130px;
   padding-left: 20px;
+
+  cursor: default;
 `;
 
 const Period = styled.div`
   width: 198px;
+
   font-size: 12px;
   text-align: center;
+
+  cursor: default;
 `;
 
 const CreatedAt = styled.div`
   width: 115px;
+
   font-size: 12px;
   text-align: center;
+
+  cursor: default;
 `;
